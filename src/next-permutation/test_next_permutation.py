26d2169fb27b6c0eb8a7a792edfada3fe8daa695
datttrian <datttrian@gmail.com>
2023-09-19 11:51:51 +0000

    Next Permutation

--- src/next-permutation/test_next_permutation.py
+++ src/next-permutation/test_next_permutation.py
@@ -1,19 +1,25 @@
 import pytest
-from solution import Solution  # Import the Solution class from your implementation
+from next_permutation import (
+    Solution,
+)  # Import the Solution class from your implementation
+
 
 class TestSolution:
-    @pytest.mark.parametrize("nums, expected", [
-        ([1, 2, 3], [1, 3, 2]),
-        ([3, 2, 1], [1, 2, 3]),
-        ([1, 1, 5], [1, 5, 1]),
-        ([1], [1]),
-        ([1, 3, 2], [2, 1, 3]),
-        ([3, 1, 2], [3, 2, 1]),
-        ([1, 2, 3, 4], [1, 2, 4, 3]),
-        ([4, 3, 2, 1], [1, 2, 3, 4]),
-        ([1, 2, 2], [2, 1, 2]),
-        ([1, 1], [1, 1]),
-    ])
+    @pytest.mark.parametrize(
+        "nums, expected",
+        [
+            ([1, 2, 3], [1, 3, 2]),
+            ([3, 2, 1], [1, 2, 3]),
+            ([1, 1, 5], [1, 5, 1]),
+            ([1], [1]),
+            ([1, 3, 2], [2, 1, 3]),
+            ([3, 1, 2], [3, 2, 1]),
+            ([1, 2, 3, 4], [1, 2, 4, 3]),
+            ([4, 3, 2, 1], [1, 2, 3, 4]),
+            ([1, 2, 2], [2, 1, 2]),
+            ([1, 1], [1, 1]),
+        ],
+    )
     def test_nextPermutation(self, nums, expected):
         solution = Solution()
         solution.nextPermutation(nums)
