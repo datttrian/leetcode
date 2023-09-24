0122c97edf0803cc70415a4df2b532f388b8e992
datttrian <datttrian@gmail.com>
2023-09-24 21:21:51 +0000

    Sun, Sep 24, 2023, 9:21:51 PM +00:00:00

--- src/two-sum/two_sum.py
+++ src/two-sum/two_sum.py
@@ -1,7 +1,15 @@
+"""
+Module docstring: This module contains a solution to the two-sum problem.
+"""
+
 from typing import List
 
 
 class Solution:
+    """
+    Class docstring: This class provides a solution to the two-sum problem.
+    """
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         numMap = {}
         n = len(nums)
