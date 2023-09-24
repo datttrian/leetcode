67f9b4675a13495c2192dcaca8eb9f2216426ced
datttrian <datttrian@gmail.com>
2023-09-24 21:58:11 +0000

    Sun, Sep 24, 2023, 9:58:11 PM +00:00:00

--- src/two-sum/two_sum.py
+++ src/two-sum/two_sum.py
@@ -2,7 +2,18 @@ from typing import List
 
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: List[int], target: int):
+        """
+        Find and return the indices of two numbers in the 'nums' list that add up to the 'target'.
+
+        Args:
+            nums (List[int]): The list of integers to search for a pair.
+            target (int): The target sum to find.
+
+        Returns:
+            List[int]: A list containing the indices of the two numbers that add up to the target.
+                       If no such pair is found, an empty list is returned.
+        """
         numMap = {}
         n = len(nums)
 
