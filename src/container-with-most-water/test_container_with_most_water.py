48ce235c04e0cac2eaeefbc7515c82b9fafe466b
datttrian <datttrian@gmail.com>
2023-10-22 14:57:01 +0000

    Sun, Oct 22, 2023, 2:57 PM +00:00

---
+++ src/container-with-most-water/test_container_with_most_water.py
@@ -0,0 +1,16 @@
+import pytest
+from container_with_most_water import Solution
+
+
+class TestSolution:
+    @pytest.mark.parametrize(
+        ('height', 'expected'),
+        [
+            ([1, 8, 6, 2, 5, 4, 8, 3, 7], 49),
+            ([4, 3, 2, 1, 4], 16),
+            ([1, 2, 1], 2),
+        ],
+    )
+    def test_maxArea(self, height, expected):
+        solution = Solution()
+        assert solution.maxArea(height) == expected
