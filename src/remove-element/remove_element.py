91b8acc77a8230ee96648274087193e37774346a
datttrian <datttrian@gmail.com>
2023-10-23 13:53:43 +0000

    Mon, Oct 23, 2023, 1:53 PM +00:00

--- src/remove-element/remove_element.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def removeElement(self, A, elem):
-        begin = 0
-        for i in range(len(A)):
-            if A[i] != elem:
-                A[begin] = A[i]
-                begin += 1
-        return begin
