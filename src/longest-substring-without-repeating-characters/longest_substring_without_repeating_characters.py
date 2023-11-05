e99cd75afb20070149be87e1fbfdc2871de0e7c4
datttrian <datttrian@gmail.com>
2023-11-05 17:09:45 +0000

    Sun, Nov 5, 2023, 5:09 PM +00:00

--- src/longest-substring-without-repeating-characters/longest_substring_without_repeating_characters.py
+++ src/longest-substring-without-repeating-characters/longest_substring_without_repeating_characters.py
@@ -1,58 +1,24 @@
-from typing import Optional
-
-
-class ListNode:
-    def __init__(
-        self,
-        val: int = 0,
-        next_node: 'Optional[ListNode]' = None,
-    ) -> None:
-        self.val: int = val
-        self.next: Optional[ListNode] = next_node
+from typing import Dict
 
 
 class Solution:
-    def addTwoNumbers(
-        self,
-        l1: ListNode | None,
-        l2: ListNode | None,
-    ) -> ListNode | None:
-        head: ListNode = ListNode(
-            0,
-        )  # dummy head node to simplify the addition
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        char_index_map: Dict[str, int] = {}
+        start = max_length = 0
 
-        current: ListNode = (
-            head  # pointer to the current node in the result list
-        )
-        carry: int = 0  # initialize carry to zero
+        for i, char in enumerate(s):
+            # If the character is found in the map and
+            # is inside the current window
+            if char in char_index_map and char_index_map[char] >= start:
+                # Move the start to one position ahead of the last occurrence
+                # to avoid repeating character
+                start = char_index_map[char] + 1
 
-        # Continue looping until both lists are exhausted and there is no carry
-        while l1 or l2 or carry:
-            val1: int = (
-                l1.val if l1 else 0
-            )  # get the value of the current node of l1, 0 if l1 is exhausted
+            # Update the last seen index of the character
+            char_index_map[char] = i
 
-            val2: int = (
-                l2.val if l2 else 0
-            )  # get the value of the current node of l2, 0 if l2 is exhausted
+            # Calculate the maximum length by comparing the current length and
+            # the previously stored maximum length
+            max_length = max(max_length, i - start + 1)
 
-            carry, out = divmod(
-                val1 + val2 + carry,
-                10,
-            )  # calculate the sum and the new carry
-
-            current.next = ListNode(
-                out,
-            )  # create a new node with the sum value
-
-            current = current.next  # move to the next node
-
-            l1 = (
-                l1.next if l1 else None
-            )  # advance to the next node in l1 if available
-
-            l2 = (
-                l2.next if l2 else None
-            )  # advance to the next node in l2 if available
-
-        return head.next
+        return max_length
