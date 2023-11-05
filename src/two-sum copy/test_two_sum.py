4ee09b50c31c53cb2ba9549e37140c16bcd3a335
datttrian <datttrian@gmail.com>
2023-11-05 16:44:39 +0000

    Sun, Nov 5, 2023, 4:44 PM +00:00

--- src/two-sum copy/test_two_sum.py
+++ src/two-sum copy/test_two_sum.py
@@ -1,17 +1,17 @@
-# from typing import List
-# import pytest
-# from two_sum import Solution
+from typing import List
+import pytest
+from two_sum import Solution
 
 
-# @pytest.mark.parametrize(
-#     ('nums', 'target', 'expected'),
-#     [
-#         ([2, 7, 11, 15], 9, [0, 1]),
-#         ([3, 2, 4], 6, [1, 2]),
-#         ([3, 3], 6, [0, 1]),
-#         # Add more test cases here
-#     ],
-# )
-# def test_twoSum(nums: List[int], target: int, expected: List[int]):
-#     solution = Solution()
-#     assert solution.twoSum(nums, target) == expected
+@pytest.mark.parametrize(
+    ('nums', 'target', 'expected'),
+    [
+        ([2, 7, 11, 15], 9, [0, 1]),
+        ([3, 2, 4], 6, [1, 2]),
+        ([3, 3], 6, [0, 1]),
+        # Add more test cases here
+    ],
+)
+def test_twoSum(nums: List[int], target: int, expected: List[int]):
+    solution = Solution()
+    assert solution.twoSum(nums, target) == expected
