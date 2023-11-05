36cc740e44eee0fd0691e52f0f361f98c385b999
datttrian <datttrian@gmail.com>
2023-11-05 16:56:40 +0000

    Sun, Nov 5, 2023, 4:56 PM +00:00

--- src/add-two-numbers/add_two_numbers.py
+++ src/add-two-numbers/add_two_numbers.py
@@ -35,6 +35,7 @@ class Solution:
             val2: int = (
                 l2.val if l2 else 0
             )  # get the value of the current node of l2, 0 if l2 is exhausted
+
             carry, out = divmod(
                 val1 + val2 + carry,
                 10,
@@ -43,13 +44,15 @@ class Solution:
             current.next = ListNode(
                 out,
             )  # create a new node with the sum value
+
             current = current.next  # move to the next node
 
             l1 = (
                 l1.next if l1 else None
             )  # advance to the next node in l1 if available
+
             l2 = (
                 l2.next if l2 else None
             )  # advance to the next node in l2 if available
 
-        return head.next  # return the sum list, excluding the dummy head node
+        return head.next
