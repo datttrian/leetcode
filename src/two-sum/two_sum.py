38a63632576f8b98843c848654a5ba9312d4b41d
datttrian <datttrian@gmail.com>
2023-11-05 14:36:50 +0000

    Sun, Nov 5, 2023, 2:36 PM +00:00

--- src/two-sum/two_sum.py
+++ src/two-sum/two_sum.py
@@ -3,13 +3,17 @@ from typing import List, Dict
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numMap: Dict[int, int] = {}  # type annotation
-        n = len(nums)
-
-        for i in range(n):
-            complement = target - nums[i]
-            if complement in numMap:
-                return [numMap[complement], i]
-            numMap[nums[i]] = i
-
-        return []  # No solution found
+        # Create a dictionary to store the numbers and their indices
+        num_dict: Dict[int, int] = {}
+        # Iterate over the list of numbers
+        for i, num in enumerate(nums):
+            # Calculate the complement of the current number
+            complement = target - num
+            # If the complement is in the dictionary, we have found a solution
+            if complement in num_dict:
+                return [num_dict[complement], i]
+            # Otherwise, add the number to the dictionary
+            num_dict[num] = i
+        # If no solution is found, this line will never be reached
+        # because the problem statement guarantees exactly one solution
+        return []
