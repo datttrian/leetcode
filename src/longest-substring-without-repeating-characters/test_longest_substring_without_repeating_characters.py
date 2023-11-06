868bf2102e365beb1e4b233e03c9f541f189aeb1
datttrian <datttrian@gmail.com>
2023-11-06 13:49:17 +0000

    Mon, Nov 6, 2023, 1:49 PM +00:00

--- src/longest-substring-without-repeating-characters/test_longest_substring_without_repeating_characters.py
+++ src/longest-substring-without-repeating-characters/test_longest_substring_without_repeating_characters.py
@@ -1,20 +1,20 @@
-# import pytest
-# from longest_substring_without_repeating_characters import Solution
+import pytest
+from longest_substring_without_repeating_characters import Solution
 
 
-# class TestSolution:
-#     @pytest.mark.parametrize(
-#         ('s', 'expected'),
-#         [
-#             ('abcabcbb', 3),
-#             ('bbbbb', 1),
-#             ('pwwkew', 3),
-#             ('', 0),
-#             ('abcdefg', 7),
-#             ('aab', 2),
-#             ('dvdf', 3),
-#         ],
-#     )
-#     def test_lengthOfLongestSubstring(self, s: str, expected: int):
-#         solution = Solution()
-#         assert solution.lengthOfLongestSubstring(s) == expected
+class TestSolution:
+    @pytest.mark.parametrize(
+        ('s', 'expected'),
+        [
+            ('abcabcbb', 3),
+            ('bbbbb', 1),
+            ('pwwkew', 3),
+            ('', 0),
+            ('abcdefg', 7),
+            ('aab', 2),
+            ('dvdf', 3),
+        ],
+    )
+    def test_lengthOfLongestSubstring(self, s: str, expected: int):
+        solution = Solution()
+        assert solution.lengthOfLongestSubstring(s) == expected
