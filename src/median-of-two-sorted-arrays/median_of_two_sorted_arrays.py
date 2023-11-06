7dd7b99b3ac8a3f2aae65b09fed550c2e757817b
datttrian <datttrian@gmail.com>
2023-11-06 14:12:37 +0000

    Mon, Nov 6, 2023, 2:12 PM +00:00

--- src/median-of-two-sorted-arrays/median_of_two_sorted_arrays.py
+++ src/median-of-two-sorted-arrays/median_of_two_sorted_arrays.py
@@ -1,44 +1,71 @@
 from typing import List
 
 
-class Solution:     
-	def findMedianSortedArrays(self, a, b):
+class Solution:
+    def findMedianSortedArrays(self, a, b):
+        """
+        Finds the median of two sorted arrays.
+        
+        The overall run time complexity should be O(log (m+n)) where m and n are the lengths of the arrays.
+        This method finds the median by utilizing a helper function to find the kth smallest element in the
+        union of both arrays.
+        
+        Args:
+        a (List[int]): The first sorted array.
+        b (List[int]): The second sorted array.
+        
+        Returns:
+        float: The median of the two sorted arrays. Returns None if both arrays are empty.
+        
+        The function handles both even and odd total lengths by computing the middle index according to
+        whether the total length `l` is odd or even.
+        """
+        l = len(a) + len(b)
 
-		l = len(a) + len(b)
+        if l == 0:  # Edge case where both arrays are empty.
+            return None
+        if l % 2:  # If the total length is odd, return the middle element.
+            return self.get_median(a, b, l // 2)
+        else:  # If the total length is even, return the average of the two middle elements.
+            return (self.get_median(a, b, l // 2) + self.get_median(a, b, l // 2 - 1)) / 2
 
-		if l == 0:
-			return None
-		if l % 2:
-			return self.get_median(a, b, l // 2)
-		else:
-			return (self.get_median(a, b, l // 2) + self.get_median(a, b, l // 2 - 1))  / 2
+    def get_median(self, a, b, idx):
+        """
+        Helper function to find the value at index `idx` in the sorted array formed by merging `a` and `b`.
+        
+        Args:
+        a (List[int]): The first sorted array, which can become empty as the recursion progresses.
+        b (List[int]): The second sorted array, which can become empty as the recursion progresses.
+        idx (int): The index for which we want to find the value in the merged array.
+        
+        Returns:
+        int: The value at index `idx` in the merged sorted array.
+        
+        This function works by comparing middle elements of `a` and `b` and recursively calling itself on
+        a subarray that must contain the kth smallest element.
+        """
+        if not a:  # If `a` is empty, return the element at `idx` from `b`.
+            return b[idx]
+        if not b:  # If `b` is empty, return the element at `idx` from `a`.
+            return a[idx]
 
-		
+        ai = len(a) // 2
+        bi = len(b) // 2
+        ma = a[ai]
+        mb = b[bi]
 
-	def get_median(self, a, b, idx):
+        # Ensure ma is the smaller median to proceed with the logic
+        if ma > mb:
+            a, b = b, a  # Swap arrays
+            ai, bi = bi, ai  # Swap indices
 
-		if not a:
-			return b[idx]
-		if not b:
-			return a[idx]
+        max_idx_ma = ai + bi  # The maximum index `ma` could be at in the merged array
 
-		ai = len(a) // 2
-		bi = len(b) // 2
-		ma = a[ai]
-		mb = b[bi]
+        # If `idx` is greater than `max_idx_ma`, we know the kth element is not in the first `ai` elements of `a`.
+        if max_idx_ma < idx:
+            return self.get_median(a[ai + 1:], b, idx - (ai + 1))
+        else:
+            # Otherwise, the kth element is not in the elements after `bi` in `b`.
+            return self.get_median(a, b[:bi], idx)
 
-		if ma > mb:
-			ma, mb = mb, ma
-			ai, bi = bi, ai
-			a, b = b, a
-
-		max_idx_ma = len(a) // 2 + len(b) // 2
-
-		if max_idx_ma < idx:
-			med = self.get_median(a[ai+1:], b, idx - (len(a)//2+1))
-		else:
-			# max_idx_ma < min_idx_mb (because they are different numbers) 
-			# => idx <= max_idx_ma <  min_idx_mb => idx < min_idx_mb
-			med = self.get_median(a, b[:bi], idx)
-
-		return med
+# Note: The function assumes that the arrays are sorted in non-decreasing order.
