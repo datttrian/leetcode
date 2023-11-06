ee143a3afc40f1e307bb6cd3ed7d8fb4a88ada92
datttrian <datttrian@gmail.com>
2023-11-06 14:08:09 +0000

    Mon, Nov 6, 2023, 2:08 PM +00:00

--- src/median-of-two-sorted-arrays/median_of_two_sorted_arrays.py
+++ src/median-of-two-sorted-arrays/median_of_two_sorted_arrays.py
@@ -2,9 +2,7 @@ from typing import List
 
 
 class Solution:
-    def findMedianSortedArrays(
-        self, nums1: List[int], nums2: List[int]
-    ) -> float:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         A, B = nums1, nums2
         total = len(nums1) + len(nums2)
         half = total // 2
@@ -22,7 +20,7 @@ class Solution:
             Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
             Bleft = B[j] if j >= 0 else float("-infinity")
             Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
-
+        
             # Partition is correct
             if Aleft <= Bright and Bleft <= Aright:
                 # Odd total length
