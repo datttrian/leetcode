9499e6debc90391a12b7cb1ec1a9cd5cf2f13853
datttrian <datttrian@gmail.com>
2023-11-07 16:34:08 +0000

    Tue, Nov 7, 2023, 4:34 PM +00:00

--- src/longest-substring-without-repeating-characters/longest_substring_without_repeating_characters.py
+++ src/longest-substring-without-repeating-characters/longest_substring_without_repeating_characters.py
@@ -3,29 +3,44 @@ from typing import Dict
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        char_index_map: Dict[
-            str,
-            int,
-        ] = {}  # Maps characters to their latest indices
-        left: int = 0  # Left boundary of the window
-        max_length: int = (
-            0  # Result to store the length of the longest substring
-        )
+        """
+        Compute the length of the longest substring without repeating
+        characters.
 
-        for right, char in enumerate(
-            s,
-        ):  # right is the right boundary of the window
-            # If the character is found in the map and the last index is
-            # within the current window
+        This function uses a sliding window approach to find the maximum
+        length of a substring without duplicate characters in a given string.
+        It iterates through the string, using a dictionary to store the most
+        recent index of each character encountered. When a repeat character is
+        found, the left boundary of the window is moved to one past the last
+        occurrence of that character.
+
+        Args:
+            s (str): The input string to be evaluated.
+
+        Returns:
+            int: The length of the longest substring without repeating
+            characters.
+
+        Complexity:
+            Time: O(n), where n is the length of the string. The function
+            iterates through the string once with constant-time operations at
+            each step.
+            Space: O(min(m, n)), where m is the size of the character set and
+            n is the length of the string. In the worst case, the whole string
+            might be stored in the dictionary, but typically the size is
+             limited by the character set.
+        """
+        char_index_map: Dict[str, int] = {}
+        left: int = 0  # Initial left boundary of the sliding window
+        max_length: int = 0  # Length of the longest substring found
+
+        for right, char in enumerate(s):
             if char in char_index_map and char_index_map[char] >= left:
-                # Move the left boundary right after the previous occurrence
-                # of the character
-                left = char_index_map[char] + 1
-            char_index_map[
-                char
-            ] = right  # Update the latest index of the character
+                left = char_index_map[char] + 1  # Adjust the left boundary
+            char_index_map[char] = right  # Update the character's index
             max_length = max(
                 max_length,
                 right - left + 1,
-            )  # Calculate the window size and update the result if it's larger
+            )  # Update max_length if current window is larger
+
         return max_length
