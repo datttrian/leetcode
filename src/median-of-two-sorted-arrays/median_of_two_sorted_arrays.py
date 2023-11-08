689480471d794325273cad532a1cc0e4e141eaab
datttrian <datttrian@gmail.com>
2023-11-08 13:03:23 +0000

    Wed, Nov 8, 2023, 1:03 PM +00:00

--- src/median-of-two-sorted-arrays/median_of_two_sorted_arrays.py
+++ src/median-of-two-sorted-arrays/median_of_two_sorted_arrays.py
@@ -45,20 +45,12 @@ class Solution:
             # Find the corresponding index j in array B using half_point
             j = half_point - i - 2
 
-            # Determine the edge values around the partition to compare, with
+            # Determine the edge values around the partition to compare
             # -infinity and +infinity as defaults for out-of-bound indices
-            Aleft = (
-                A[i] if i >= 0 else float('-infinity')
-            )  # Left partition of A
-            Aright = (
-                A[i + 1] if (i + 1) < len(A) else float('infinity')
-            )  # Right partition of A
-            Bleft = (
-                B[j] if j >= 0 else float('-infinity')
-            )  # Left partition of B
-            Bright = (
-                B[j + 1] if (j + 1) < len(B) else float('infinity')
-            )  # Right partition of B
+            Aleft = A[i] if i >= 0 else float('-infinity')
+            Aright = A[i + 1] if (i + 1) < len(A) else float('infinity')
+            Bleft = B[j] if j >= 0 else float('-infinity')
+            Bright = B[j + 1] if (j + 1) < len(B) else float('infinity')
 
             # Check if we have found a valid partition
             if Aleft <= Bright and Bleft <= Aright:
