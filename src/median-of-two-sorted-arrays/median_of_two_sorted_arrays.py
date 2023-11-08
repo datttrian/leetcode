bc72bf1d65c4649ca0cba94215ade0b8cb87353c
datttrian <datttrian@gmail.com>
2023-11-08 12:57:54 +0000

    Wed, Nov 8, 2023, 12:57 PM +00:00

--- src/median-of-two-sorted-arrays/median_of_two_sorted_arrays.py
+++ src/median-of-two-sorted-arrays/median_of_two_sorted_arrays.py
@@ -28,7 +28,7 @@ class Solution:
             the smaller array.
             Space: O(1), as the solution uses constant extra space.
         """
-        # Assign nums1 and nums2 to A and B with A being the smaller array
+        # Assign nums1 and nums to A and B with len(A) <= len(B)
         A, B = (nums1, nums2) if len(nums1) <= len(nums2) else (nums2, nums1)
 
         # Calculate the total length of both arrays and the middle index
@@ -38,42 +38,36 @@ class Solution:
         # Initialize the binary search on the smaller array A
         left_index, right_index = 0, len(A) - 1
 
-        # Perform a binary search to find the correct partition
+        # Initialize a binary search to find the correct partition
         while True:
-            # Find the index i in array A such that it is approximately the
-            # median
-            i = (left_index + right_index) // 2  # index in A
-            # Find the corresponding index j in array B using the half_point
-            j = half_point - i - 2  # index in B
+            # Find the index i in array A such that it is close to the median
+            i = (left_index + right_index) // 2
+            # Find the corresponding index j in array B using half_point
+            j = half_point - i - 2
 
-            # Determine the edge values around the partition to compare, with
+            # Determine the edge values around the partition to compare
             # -infinity and +infinity as defaults for out-of-bound indices
-            Aleft = (
-                A[i] if i >= 0 else float('-infinity')
-            )  # Left partition of A
-            Aright = (
-                A[i + 1] if (i + 1) < len(A) else float('infinity')
-            )  # Right partition of A
-            Bleft = (
-                B[j] if j >= 0 else float('-infinity')
-            )  # Left partition of B
-            Bright = (
-                B[j + 1] if (j + 1) < len(B) else float('infinity')
-            )  # Right partition of B
+            Aleft = A[i] if i >= 0 else float('infinity')
+            Aright = A[i + 1] if (i + 1) < len(A) else float('infinity')
+            Bleft = B[j] if j >= 0 else float('infinity')
+            Bright = B[j + 1] if (j + 1) < len(B) else float('infinity')
 
             # Check if we have found a valid partition
             if Aleft <= Bright and Bleft <= Aright:
                 # For odd total length, the median is the first middle element
                 if total_length % 2:
                     return min(Aright, Bright)
-                # For even total length, the median is the average of the two
+
+                # Fir even total length, the median is the average of the two
                 # middle elements
                 return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+
             # If elements on the left side of A are greater, move the
-            # partition left
+            # partition to the left
             elif Aleft > Bright:
-                right_index = i - 1  # Move partition left in A
+                right_index = i - 1
+
             # If elements on the left side of B are greater, move the
-            # partition right
+            # partition to the right
             else:
-                left_index = i + 1  # Move partition right in A
+                left_index = i + 1
