3037c12f7f10ec65b5deeb8707505d347c97792d
datttrian <datttrian@gmail.com>
2023-11-08 13:33:21 +0000

    Wed, Nov 8, 2023, 1:33 PM +00:00

--- src/median-of-two-sorted-arrays/median_of_two_sorted_arrays.py
+++ src/median-of-two-sorted-arrays/median_of_two_sorted_arrays.py
@@ -7,67 +7,49 @@ class Solution:
         nums1: List[int],
         nums2: List[int],
     ) -> float:
-        """
-        Find the median of two sorted arrays.
+        # Make sure nums1 is the smaller array
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
 
-        This function takes two sorted arrays and finds their median as if
-        they were part of the same sorted array. It uses a binary search
-        algorithm to find the correct partition between the arrays that would
-        form a merged sorted array, and calculates the median from the
-        elements around the partition.
+        m: int = len(nums1)
+        n: int = len(nums2)
+        imin: int = 0
+        imax: int = m
+        half_len: int = (m + n + 1) // 2
 
-        Args:
-            nums1 (List[int]): The first sorted array.
-            nums2 (List[int]): The second sorted array.
+        while imin <= imax:
+            i: int = (imin + imax) // 2
+            j: int = half_len - i
 
-        Returns:
-            float: The median of the two sorted arrays.
-
-        Complexity:
-            Time: O(log(min(m, n))), because the binary search is performed on
-            the smaller array.
-            Space: O(1), as the solution uses constant extra space.
-        """
-        # Assign nums1 and nums to A and B with len(A) <= len(B)
-        A, B = (nums1, nums2) if len(nums1) <= len(nums2) else (nums2, nums1)
-
-        # Calculate the total length of both arrays and the middle index
-        total_length = len(A) + len(B)
-        half_point = total_length // 2
-
-        # Initialize the binary search on the smaller array A
-        left_index, right_index = 0, len(A) - 1
-
-        # Perform a binary search to find the correct partition
-        while True:
-            # Find the index i in array A such that it is close to the median
-            i = (left_index + right_index) // 2
-            # Find the corresponding index j in array B using half_point
-            j = half_point - i - 2
-
-            # Determine the edge values around the partition to compare
-            # -infinity and +infinity as defaults for out-of-bound indices
-            Aleft = A[i] if i >= 0 else float('-infinity')
-            Aright = A[i + 1] if (i + 1) < len(A) else float('infinity')
-            Bleft = B[j] if j >= 0 else float('-infinity')
-            Bright = B[j + 1] if (j + 1) < len(B) else float('infinity')
-
-            # Check if we have found a valid partition
-            if Aleft <= Bright and Bleft <= Aright:
-                # For odd total length, the median is the first middle element
-                if total_length % 2:
-                    return min(Aright, Bright)
-
-                # For even total length, the median is the average of the two
-                # middle elements
-                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
-
-            # If elements on the left side of A are greater, move the
-            # partition to the left
-            elif Aleft > Bright:
-                right_index = i - 1
-
-            # If elements on the left side of B are greater, move the
-            # partition to the right
+            if i < m and nums2[j - 1] > nums1[i]:
+                # i is too small, must increase it
+                imin = i + 1
+            elif i > 0 and nums1[i - 1] > nums2[j]:
+                # i is too big, must decrease it
+                imax = i - 1
             else:
-                left_index = i + 1
+                # i is perfect
+                if i == 0:
+                    max_of_left: int = nums2[j - 1]
+                elif j == 0:
+                    max_of_left = nums1[i - 1]
+                else:
+                    max_of_left = max(nums1[i - 1], nums2[j - 1])
+
+                if (m + n) % 2 == 1:
+                    return float(max_of_left)
+
+                if i == m:
+                    min_of_right: int = nums2[j]
+                elif j == n:
+                    min_of_right = nums1[i]
+                else:
+                    min_of_right = min(nums1[i], nums2[j])
+
+                return (max_of_left + min_of_right) / 2.0
+
+        # Fallback return statement, this should logically never be reached
+        # due to the nature of the problem.
+        # But it's required to satisfy static type checking and ensure
+        # the function signature contract (always returning a float) is met.
+        return 0.0
