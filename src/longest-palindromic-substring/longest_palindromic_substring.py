57ce202bd1ee8df7ae3bae1559bacdbee0a891fc
datttrian <datttrian@gmail.com>
2023-11-09 21:01:03 +0000

    Thu, Nov 9, 2023, 9:01 PM +00:00

--- src/longest-palindromic-substring/longest_palindromic_substring.py
+++ src/longest-palindromic-substring/longest_palindromic_substring.py
@@ -32,25 +32,37 @@ class Solution:
         resLen = 0
 
         for i in range(len(s)):
-            # Check for palindrome of odd length by setting both left and
-            # right pointers at i
+            # For each character in the string, attempt to find the longest
+            # palindrome with the character as the center for odd-length
+            # palindromes.
             l, r = i, i
             while l >= 0 and r < len(s) and s[l] == s[r]:
+                # If the currently considered substring is a palindrome
+                # (indicated by the equality of characters at positions l and
+                # r), and if its length is greater than the length of the
+                # previously recorded longest palindrome, then update the
+                # result and the length of the result
                 if (r - l + 1) > resLen:
                     res = s[l : r + 1]
                     resLen = r - l + 1
+                # Expand the considered range to the left and right
                 l -= 1
                 r += 1
 
-            # Check for palindrome of even length by setting left at i and
-            # right at i + 1
+            # After checking for odd-length palindromes with center `i`, check
+            # for even-length palindromes by considering the center to be
+            # between `i` and `i+1`.
             l, r = i, i + 1
             while l >= 0 and r < len(s) and s[l] == s[r]:
+                # Similar to the odd-length palindrome check, update the
+                # result and its length if a longer palindrome is found.
                 if (r - l + 1) > resLen:
                     res = s[l : r + 1]
                     resLen = r - l + 1
-
+                # Expand the considered range to the left and right
                 l -= 1
                 r += 1
 
-        return res  # Return the longest palindrome
+        # After checking all characters and all possible centers for
+        # palindromes, return the longest palindrome found in the string.
+        return res
