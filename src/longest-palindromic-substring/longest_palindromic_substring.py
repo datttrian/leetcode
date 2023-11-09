acfa10bf24d1c4c15e2f5faba958e51874578c99
datttrian <datttrian@gmail.com>
2023-11-09 20:49:29 +0000

    Thu, Nov 9, 2023, 8:49 PM +00:00

--- src/longest-palindromic-substring/longest_palindromic_substring.py
+++ src/longest-palindromic-substring/longest_palindromic_substring.py
@@ -1,25 +1,53 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res = ''
-        resLen = 0
+        """
+        Find the longest palindromic substring in the given string.
+
+        This algorithm, using the Expand Around Center technique, considers
+        each index in the string as the middle of a potential palindrome
+        and expands outwards checking for palindrome conditions. It expands
+        around each character (for odd-length palindromes) and each pair of
+        consecutive characters (for even-length palindromes), updating the
+        longest palindrome found.
+
+
+        Args:
+            s (str): The input string to search for the palindrome.
+
+        Returns:
+            str: The longest palindromic substring in the input string.
+
+        Complexity:
+            Time: O(n^2), where n is the length of the input string. This time
+            complexity arises from the fact that for each of the n characters,
+            the expansion could potentially go through the rest of the string.
+
+            Space: O(1), as the space used does not depend on the size of the
+            input string and only a constant amount of extra space is used.
+        """
+        res = ''  # The result string to hold the longest palindrome found
+        resLen = 0  # The length of the longest palindrome found
 
         for i in range(len(s)):
-            # Odd length
+            # Check for palindrome of odd length by setting both left and
+            # right pointers at i
             l, r = i, i
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 if (r - l + 1) > resLen:
                     res = s[l : r + 1]
                     resLen = r - l + 1
-                l -= 1
-                r += 1
+                l -= 1  # Expand to the left
+                r += 1  # Expand to the right
 
-            # even length
+            # Check for palindrome of even length by setting left at i and
+            # right at i + 1
             l, r = i, i + 1
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 if (r - l + 1) > resLen:
                     res = s[l : r + 1]
                     resLen = r - l + 1
 
-                l -= 1
-                r += 1
-        return res
+                l -= 1  # Expand to the left
+                r += 1  # Expand to the right
+
+        return res  # Return the longest palindrome
