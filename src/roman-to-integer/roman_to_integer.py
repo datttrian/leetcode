dbbf3f5939c967d3a6103618e670f169d50ac610
datttrian <datttrian@gmail.com>
2023-11-11 13:57:19 +0000

    Sat, Nov 11, 2023, 1:57 PM +00:00

---
+++ src/roman-to-integer/roman_to_integer.py
@@ -0,0 +1,23 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+        result = 0
+        prev_value = 0
+
+        for char in reversed(s):
+            current_value = roman_dict[char]
+            if current_value < prev_value:
+                result -= current_value
+            else:
+                result += current_value
+            prev_value = current_value
+
+        return result
