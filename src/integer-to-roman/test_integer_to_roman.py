52dcb99cea4f29feeb41ecbf56d6e17d661445aa
datttrian <datttrian@gmail.com>
2023-11-11 13:49:47 +0000

    Sat, Nov 11, 2023, 1:49 PM +00:00

---
+++ src/integer-to-roman/test_integer_to_roman.py
@@ -0,0 +1,21 @@
+import pytest
+from integer_to_roman import (
+    Solution,
+)  # Assuming your solution is in a module named solution_module
+
+
+@pytest.mark.parametrize(
+    ('num', 'expected_roman'),
+    [
+        (1, 'I'),
+        (4, 'IV'),
+        (9, 'IX'),
+        (58, 'LVIII'),
+        (1994, 'MCMXCIV'),
+        # Add more test cases as needed
+    ],
+)
+def test_intToRoman(num: int, expected_roman: str):
+    solution = Solution()
+    result = solution.intToRoman(num)
+    assert result == expected_roman
