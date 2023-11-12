147045285cf12599eace1f979726a7fed19b8ce9
datttrian <datttrian@gmail.com>
2023-11-12 19:11:12 +0000

    Sun, Nov 12, 2023, 7:11 PM +00:00

--- src/3sum/three_sum.py
+++ src/3sum/three_sum.py
@@ -1,15 +1,16 @@
-from typing import List
+from typing import List, Tuple
+
 
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums: List[int]) -> List[Tuple[int, int, int]]:
         nums.sort()
-        result = []
-    
+        result: List[Tuple[int, int, int]] = []  # type
+
         for i in range(len(nums) - 2):
             # Avoid duplicates for the first element of the triplet
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-    
+
             left, right = i + 1, len(nums) - 1
             while left < right:
                 total = nums[i] + nums[left] + nums[right]
@@ -18,13 +19,14 @@ class Solution:
                 elif total > 0:
                     right -= 1
                 else:
-                    result.append([nums[i], nums[left], nums[right]])
-                    # Skip duplicates for the second and third elements of the triplet
+                    result.append((nums[i], nums[left], nums[right]))
+                    # Skip duplicates for the second and third elements of the
+                    # triplet
                     while left < right and nums[left] == nums[left + 1]:
                         left += 1
                     while left < right and nums[right] == nums[right - 1]:
                         right -= 1
                     left += 1
                     right -= 1
-    
+
         return result
