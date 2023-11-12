7f1e9e2ab2860e9501f9bfe79923158d86c43dc8
datttrian <datttrian@gmail.com>
2023-11-12 19:13:06 +0000

    Sun, Nov 12, 2023, 7:13 PM +00:00

---
+++ src/3sum/test_three_sum.py
@@ -0,0 +1,21 @@
+from typing import List, Tuple
+import pytest
+from three_sum import (
+    Solution,
+)  # Assuming your solution class is in a module called solution_module
+
+
+@pytest.mark.parametrize(
+    ('nums', 'expected'),
+    [
+        ([-1, 0, 1, 2, -1, -4], [[-1, -1, 2], [-1, 0, 1]]),
+        ([], []),
+        ([0], []),
+        ([1, 2], []),
+        ([-2, 0, 1, 1, 2], [[-2, 0, 2], [-2, 1, 1]]),
+        ([-2, 0, 0, 2, 2], [[-2, 0, 2]]),
+    ],
+)
+def test_threeSum(nums: List[int], expected: List[Tuple[int, int, int]]):
+    solution = Solution()
+    assert solution.threeSum(nums) == expected
