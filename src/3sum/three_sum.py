dec69aa2db3145879a27c982881b03f5b5510a11
datttrian <datttrian@gmail.com>
2023-11-12 18:55:40 +0000

    Sun, Nov 12, 2023, 6:55 PM +00:00

--- src/3sum/three_sum.py
+++ src/3sum/three_sum.py
@@ -1,5 +1,3 @@
-from typing import List
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
@@ -19,7 +17,7 @@ class Solution:
                     right -= 1
                 else:
                     result.append([nums[i], nums[left], nums[right]])
-                    # Avoid duplicates for the first element of the triplet# Avoid duplicates for the first element of the triplet# Skip duplicates for the second and third elements of the triplet
+                    # Skip duplicates for the second and third elements of the triplet
                     while left < right and nums[left] == nums[left + 1]:
                         left += 1
                     while left < right and nums[right] == nums[right - 1]:
