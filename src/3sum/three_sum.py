38a94fa457ee91dce97ce65b853eff9da16df12c
datttrian <datttrian@gmail.com>
2023-11-12 18:54:45 +0000

    Sun, Nov 12, 2023, 6:54 PM +00:00

---
+++ src/3sum/three_sum.py
@@ -0,0 +1,28 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+    
+        for i in range(len(nums) - 2):
+            # Avoid duplicates for the first element of the triplet
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+    
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    result.append([nums[i], nums[left], nums[right]])
+                    # Skip duplicates for the second and third elements of the triplet
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+    
+        return result
