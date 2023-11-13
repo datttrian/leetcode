04d1aab519e7b28d6de4b0537fca014d7dbff7e6
datttrian <datttrian@gmail.com>
2023-11-13 18:12:03 +0000

    Mon, Nov 13, 2023, 6:12 PM +00:00

---
+++ src/letter-combinations-of-a-phone-number/test_letter_combinations_of_a_phone_number.py
@@ -0,0 +1,61 @@
+import pytest
+from typing import List
+from letter_combinations_of_a_phone_number import Solution
+
+
+@pytest.mark.parametrize(
+    ('digits', 'expected'),
+    [
+        ('23', ['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf']),
+        (
+            '',
+            [],
+        ),  # Test case for an empty input, should return an empty list
+        ('2', ['a', 'b', 'c']),
+        ('7', ['p', 'q', 'r', 's']),
+        ('9', ['w', 'x', 'y', 'z']),
+        (
+            '234',
+            [
+                'adg',
+                'adh',
+                'adi',
+                'aeg',
+                'aeh',
+                'aei',
+                'afg',
+                'afh',
+                'afi',
+                'bdg',
+                'bdh',
+                'bdi',
+                'beg',
+                'beh',
+                'bei',
+                'bfg',
+                'bfh',
+                'bfi',
+                'cdg',
+                'cdh',
+                'cdi',
+                'ceg',
+                'ceh',
+                'cei',
+                'cfg',
+                'cfh',
+                'cfi',
+            ],
+        ),  # Test case for multiple digits
+        (
+            '0',
+            [],
+        ),  # Test case for a digit '0', should return an empty list
+        (
+            '1',
+            [],
+        ),  # Test case for a digit '1', should return an empty list
+    ],
+)
+def test_letterCombinations(digits: str, expected: List[str]):
+    solution = Solution()
+    assert solution.letterCombinations(digits) == expected
