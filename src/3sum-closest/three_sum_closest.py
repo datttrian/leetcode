8d7e01a620b586297f44dfaa42b37f6fa00a92f1
datttrian <datttrian@gmail.com>
2023-11-13 17:52:56 +0000

    Mon, Nov 13, 2023, 5:52 PM +00:00

---
+++ src/3sum-closest/three_sum_closest.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> float:
+        nums.sort()
+        closest_sum = float('inf')
+
+        for i in range(len(nums) - 2):
+            left, right = i + 1, len(nums) - 1
+
+            while left < right:
+                current_sum = nums[i] + nums[left] + nums[right]
+                if abs(target - current_sum) < abs(target - closest_sum):
+                    closest_sum = current_sum
+
+                if current_sum < target:
+                    left += 1
+                elif current_sum > target:
+                    right -= 1
+                else:
+                    return closest_sum
+
+        return closest_sum
