a2384fb5306b0c87cf43786d3c4976719cda8e55
datttrian <datttrian@gmail.com>
2023-11-14 06:56:36 +0000

    Tue, Nov 14, 2023, 6:56 AM +00:00

--- src/letter-combinations-of-a-phone-number/letter_combinations_of_a_phone_number.py
+++ src/letter-combinations-of-a-phone-number/letter_combinations_of_a_phone_number.py
@@ -3,6 +3,40 @@ from typing import List
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        """
+        Generate all possible letter combinations for a given string of digits.
+
+        Summary:
+        Given a string of digits, this method produces all possible
+        combinations of letters that can be formed by mapping each digit to
+        its respective set of letters on a phone keypad.
+
+        Description:
+        This method iterates through each digit in the input string, using a
+        mapping of digits to letters on a phone keypad. For each digit, it
+        generates new combinations by appending each letter to the existing
+        combinations. The final result is a list of all possible letter
+        combinations.
+
+        Algorithm Parameters:
+        - digits (str): The input string consisting of digits for which letter
+                       combinations need to be generated.
+
+        Returns:
+        List[str]: A list of strings representing all possible letter
+        combinations.
+
+        Raises:
+        - None
+
+        Complexity:
+        - Time: O(4^n), where n is the length of the input digits string. In
+        the worst case, each digit can contribute up to 4 letters (7 and 9
+        have 4 letters each). The method iterates through each digit,
+        resulting in an exponential time complexity.
+        - Space: O(4^n), as the space complexity is also exponential due to
+        the number of combinations generated.
+        """
         if not digits:
             return []
 
@@ -16,14 +50,25 @@ class Solution:
             '8': 'tuv',
             '9': 'wxyz',
         }
+        # Initialize the combinations list with an empty string as a starting
+        # point
+        combinations = ['']
 
-        combinations = ['']  # start with an empty combination
-
+        # Iterate through each digit in the input
         for digit in digits:
+            # Create a new list to store combinations for the current digit
             new_combinations: List[str] = []
+
+            # Iterate through existing combinations
             for combination in combinations:
+                # Append each letter corresponding to the current digit to the
+                # existing combinations
                 for letter in phone_map[digit]:
                     new_combinations.append(combination + letter)
+
+            # Update the combinations list with the newly generated
+            # combinations for the current digit
             combinations = new_combinations
 
+        # Return the final list of combinations
         return combinations
