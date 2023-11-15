5686ad21b47158b160acd48d6d19b77f64acdc04
datttrian <datttrian@gmail.com>
2023-11-15 12:37:21 +0000

    Wed, Nov 15, 2023, 12:37 PM +00:00

--- src/remove-nth-node-from-end-of-list/test_remove_nth_node_from_end_of_list.py
+++ src/remove-nth-node-from-end-of-list/test_remove_nth_node_from_end_of_list.py
@@ -1,43 +1,50 @@
-from typing import Optional
+from typing import List, Optional
 import pytest
 from remove_nth_node_from_end_of_list import Solution, ListNode
 
 
+# Helper function to convert a list to a linked list
+def list_to_linked_list(lst: List[int]) -> Optional[ListNode]:
+    if not lst:
+        return None
+    head = ListNode(lst[0])
+    current = head
+    for num in lst[1:]:
+        current.next = ListNode(num)
+        current = current.next
+    return head
+
+
+# Helper function to convert a linked list to a list
+def linked_list_to_list(head: Optional[ListNode]) -> List[int]:
+    result: List[int] = []
+    while head:
+        result.append(head.val)
+        head = head.next
+    return result
+
+
 @pytest.mark.parametrize(
-    ('input_list', 'n', 'expected_output'),
+    ('input_list', 'n', 'expected_list'),
     [
-        (
-            ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))),
-            2,
-            ListNode(1, ListNode(2, ListNode(3, ListNode(5)))),
-        ),
-        (ListNode(1), 1, None),  # Edge case: Removing the only element
-        (
-            ListNode(1, ListNode(2)),
-            1,
-            ListNode(1),
-        ),  # Edge case: Removing the last element
-        (
-            ListNode(1, ListNode(2)),
-            2,
-            ListNode(2),
-        ),  # Edge case: Removing the first element
+        ([1, 2, 3, 4, 5], 2, [1, 2, 3, 5]),
+        ([1, 2, 3, 4, 5], 1, [1, 2, 3, 4]),
+        ([1, 2], 2, [2]),
+        ([1], 1, []),
+        ([], 1, []),
     ],
 )
 def test_removeNthFromEnd(
-    input_list: Optional[ListNode],
+    input_list: List[int],
     n: int,
-    expected_output: Optional[ListNode],
+    expected_list: List[int],
 ):
     solution = Solution()
-    new_head = solution.removeNthFromEnd(input_list, n)
-    assert list_equal(new_head, expected_output)
+    head = list_to_linked_list(input_list)
+    expected_head = list_to_linked_list(expected_list)
 
+    result_head = solution.removeNthFromEnd(head, n)
 
-def list_equal(list1: Optional[ListNode], list2: Optional[ListNode]):
-    while list1 and list2:
-        if list1.val != list2.val:
-            return False
-        list1 = list1.next
-        list2 = list2.next
-        return list1 is None and list2 is None
+    assert linked_list_to_list(result_head) == linked_list_to_list(
+        expected_head,
+    )
