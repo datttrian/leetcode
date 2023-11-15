60a125e9137194aeaa061559de3399cbd22c6183
datttrian <datttrian@gmail.com>
2023-11-15 12:48:13 +0000

    Wed, Nov 15, 2023, 12:48 PM +00:00

---
+++ src/valid-parentheses/test_valid_parentheses.py
@@ -0,0 +1,24 @@
+import pytest
+from valid_parentheses import (
+    Solution,
+)  # Import the Solution class from your module
+
+
+@pytest.mark.parametrize(
+    ('input_str', 'expected'),
+    [
+        ('()', True),  # Simple valid case
+        ('()[]{}', True),  # Multiple types of brackets
+        ('(]', False),  # Incorrect closing bracket
+        ('([)]', False),  # Incorrect nesting
+        ('{[]}', True),  # Nested brackets
+        ('', True),  # Empty string is valid
+        ('[', False),  # Unbalanced opening bracket
+        (']', False),  # Unbalanced closing bracket
+        ('(', False),  # Unbalanced opening bracket
+        (')', False),  # Unbalanced closing bracket
+    ],
+)
+def test_is_valid(input_str: str, expected: bool):
+    solution = Solution()
+    assert solution.isValid(input_str) == expected
