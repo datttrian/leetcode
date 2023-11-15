d399c49b52aa432d1eb38f8097e0e8c1870a2d52
datttrian <datttrian@gmail.com>
2023-11-15 18:58:04 +0000

    Wed, Nov 15, 2023, 6:58 PM +00:00

--- src/generate-parentheses/generate_parentheses.py
+++ src/generate-parentheses/generate_parentheses.py
@@ -4,66 +4,78 @@ from typing import List
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         """
-        Generate all valid combinations of parentheses for a given number of
+        Generate all valid combinations of parentheses with a given number of
         pairs.
 
         Summary:
-        Generate all valid combinations of parentheses for a given number of
+        Generates all valid combinations of parentheses for a given number of
         pairs.
 
         Description:
-        This function uses a backtracking algorithm to explore all possible
-        combinations of parentheses. It ensures that at any point in the
-        construction of a combination, the number of open parentheses does not
-        exceed the given total 'n', and the number of closed parentheses does
-        not exceed the number of open parentheses. The result is a list of
-        strings representing all valid combinations of parentheses.
+        This method uses a backtracking algorithm to explore and construct all
+        possible combinations of well-formed parentheses. It maintains counts
+        of open and closed parentheses to ensure validity and completeness of
+        the generated combinations.
 
         Algorithm:
-        The backtracking algorithm explores different paths, adding an open
-        parenthesis when the count of open parentheses is less than 'n', and
-        adding a closed parenthesis when the count of closed parentheses is
-        less than the count of open parentheses. The process continues
-        recursively until a valid combination of length '2 * n' is formed.
+        1. Start with an empty string and counts of open and closed
+        parentheses set to zero.
+        2. Recursively explore two possibilities:
+           a. Add an open parenthesis if the count of open parentheses used is
+           less than n.
+           b. Add a closed parenthesis if the count of closed parentheses used
+           is less than the count of open parentheses.
+        3. Base case: When the length of the combination is equal to twice the
+        number of pairs, add it to the result.
+        4. Repeat the process until all valid combinations are generated.
 
         Parameters:
-        - n (int): The number of pairs of parentheses to generate combinations
-        for.
+        - n (int): The number of pairs of parentheses to be used.
 
         Returns:
         List[str]: A list of strings representing all valid combinations of
         parentheses.
 
         Raises:
-        N/A
+        No explicit exceptions are raised.
 
         Complexity:
-        The time complexity of this algorithm is O(4^n / sqrt(n)), where 'n'
-        is the given number of pairs. The space complexity is O(4^n / sqrt(n))
-        as well, considering the number of valid combinations that need to be
-        stored.
+        - Time: O(4^n / sqrt(n)) where n is the number of pairs, as each valid
+        combination can have up to 2n characters,
+          and at each step, we make two recursive calls.
+        - Space: O(4^n / sqrt(n)) as there can be a total of 4^n / sqrt(n)
+        valid combinations in the output list.
         """
 
         def backtrack(s: str = '', left: int = 0, right: int = 0):
             """
-            Recursive helper function to generate valid combinations of
-            parentheses.
+            Backtrack to generate valid combinations of parentheses.
 
             Parameters:
-            - s (str): The current combination being constructed.
-            - left (int): The count of open parentheses in the current
-            combination.
-            - right (int): The count of closed parentheses in the current
-            combination.
+            - s (str): The current combination of parentheses.
+            - left (int): The count of open parentheses used so far.
+            - right (int): The count of closed parentheses used so far.
             """
+            # Base case: If the combination is complete, add it to the result
             if len(s) == 2 * n:
                 result.append(s)
                 return
+
+            # Recursive cases:
+            # 1. Add an open parenthesis if the count of open parentheses used
+            # is less than n
             if left < n:
                 backtrack(s + '(', left + 1, right)
+            # 2. Add a closed parenthesis if the count of closed parentheses
+            # used is less than the count of open parentheses
             if right < left:
                 backtrack(s + ')', left, right + 1)
 
-        result: List[str] = []  # List to store the generated combinations
-        backtrack()  # Start the backtracking process
-        return result  # Return the list of valid combinations
+        # List to store valid combinations of parentheses
+        result: List[str] = []
+
+        # Start the backtracking process
+        backtrack()
+
+        # Return the final list of valid combinations
+        return result
