b9ca685a819107a837e370a700c9270ba28478a8
datttrian <datttrian@gmail.com>
2023-11-15 18:26:26 +0000

    Wed, Nov 15, 2023, 6:26 PM +00:00

---
+++ src/merge-two-sorted-lists/merge_two_sorted_lists.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(
+        self,
+        val: int = 0,
+        next_node: 'Optional[ListNode]' = None,
+    ) -> None:
+        self.val: int = val
+        self.next: Optional[ListNode] = next_node
+
+
+class Solution:
+    def mergeTwoLists(
+        self,
+        list1: Optional[ListNode],
+        list2: Optional[ListNode],
+    ) -> Optional[ListNode]:
+        dummy = ListNode()
+        current = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+
+            current = current.next
+
+        # If one of the lists is not empty, append the remaining nodes
+        if list1:
+            current.next = list1
+        elif list2:
+            current.next = list2
+
+        return dummy.next
