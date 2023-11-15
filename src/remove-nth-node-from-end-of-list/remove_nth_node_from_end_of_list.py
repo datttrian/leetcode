8eb5038684cf05def2e36205401061ceb5023865
datttrian <datttrian@gmail.com>
2023-11-15 12:49:32 +0000

    Wed, Nov 15, 2023, 12:49 PM +00:00

--- src/remove-nth-node-from-end-of-list/remove_nth_node_from_end_of_list.py
+++ src/remove-nth-node-from-end-of-list/remove_nth_node_from_end_of_list.py
@@ -60,23 +60,29 @@ class Solution:
         list.
         - Space complexity: O(1), as only constant extra space is used.
         """
+        # Create a dummy node to handle edge cases
         dummy: 'ListNode' = ListNode(0)
         dummy.next = head
 
+        # Initialize two pointers, fast and slow, to the dummy node
         fast: Optional[ListNode] = dummy
         slow: Optional[ListNode] = dummy
 
+        # Move fast pointer n + 1 steps ahead
         for _ in range(n + 1):
             if fast is not None:
                 fast = fast.next
 
+        # Move both pointers until the fast pointer reaches the end
         while fast is not None:
             fast = fast.next
             if slow is not None:
                 slow = slow.next
 
+        # Remove the nth node from the end
         if slow is not None:
             if slow.next is not None:
                 slow.next = slow.next.next
 
+        # Return the modified list starting from the dummy node's next
         return dummy.next
