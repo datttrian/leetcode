70716c92369783f5fe309ef749a3b68671d8ed4a
datttrian <datttrian@gmail.com>
2023-11-16 09:07:20 +0000

    Thu, Nov 16, 2023, 9:07 AM +00:00

---
+++ src/merge-k-sorted-lists/merge_k_sorted_lists.py
@@ -0,0 +1,35 @@
+import heapq
+from typing import Optional, List
+
+
+class ListNode:
+    def __init__(
+        self,
+        val: int = 0,
+        next_node: 'Optional[ListNode]' = None,
+    ) -> None:
+        self.val: int = val
+        self.next: Optional[ListNode] = next_node
+
+ 
+class Solution:
+    def mergeKLists(self,
+                    lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        
+        heap: List[tuple[int, int, ListNode]] = []
+        for i, l in enumerate(lists):
+            if l:
+                heapq.heappush(heap, (l.val, i, l))
+
+        dummy = ListNode()
+        current = dummy
+
+        while heap:
+            val, i, node = heapq.heappop(heap)
+            current.next = ListNode(val)
+            current = current.next
+
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
+
+        return dummy.next        
