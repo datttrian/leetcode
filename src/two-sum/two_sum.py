88f6cdb6f3aa1573d983bc90ee619034aa2a290a
datttrian <datttrian@gmail.com>
2023-11-17 14:31:10 +0000

    Fri, Nov 17, 2023, 2:31 PM +00:00

--- src/two-sum/two_sum.py
+++ src/two-sum/two_sum.py
@@ -7,13 +7,14 @@ class Solution:
         Find two indices in an integer array whose values sum up to a given
         target number.
 
-        The function iterates through the list of numbers, using a dictionary
-        to store the numbers and their corresponding indices. For each number,
-        it checks whether there is a complement in the table (i.e., a number
-        which added to the current number equals the target). If such a
-        complement is found, the function returns a list containing the indices
-        of the two numbers. If no two numbers add up to the target, the
-        function returns an empty list.
+        The function uses a hash table to store the numbers and their
+        corresponding indices. It iterates through the list of numbers only
+        once, checking for each number whether there is a complement in the
+        hash table (i.e., a number which, when added to the current number,
+        equals the target). If such a complement is found, the function
+        immediately returns a list containing the indices of the two numbers.
+        If no two numbers add up to the target, the function returns an empty
+        list.
 
         Parameters:
         - nums (List[int]): List of integers to search within.
@@ -26,6 +27,16 @@ class Solution:
         Raises:
         - This function assumes that there is exactly one solution, and it
         does not handle the case where no solution exists.
+
+        Algorithm:
+        - Create a dictionary (`num_dict`) to store the numbers and their
+        indices.
+        - Iterate over the list of numbers.
+        - For each number, calculate the complement of the current number by
+        subtracting it from the target.
+        - If the complement is in the dictionary, return the indices of the
+        two numbers adding up to the target.
+        - If no solution is found, return an empty list.
 
         Complexity:
         - Time: O(n), where n is the number of elements in `nums`. The
