1e6fa9ea7b038d94f314fd4b9d8266edc79af82a
datttrian <datttrian@gmail.com>
2023-11-17 14:35:13 +0000

    Fri, Nov 17, 2023, 2:35 PM +00:00

--- src/two-sum/two_sum.py
+++ src/two-sum/two_sum.py
@@ -4,9 +4,11 @@ from typing import List, Dict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         """
+        Summary:
         Find two indices in an integer array whose values sum up to a given
         target number.
 
+        Intuition:
         The problem requires finding two indices in an integer array whose
         values sum up to a given target number. One intuitive approach is to
         iterate through the array and, for each element, check if there is
@@ -15,10 +17,23 @@ class Solution:
         the numbers and their corresponding indices as we iterate through the
         array.
 
+        Approach:
+        - Create a dictionary (`num_dict`) to store numbers and their indices.
+        - Iterate over the list of numbers.
+        - For each number, calculate the complement by subtracting it from the
+        target.
+        - If the complement is in the dictionary, return the indices of the
+        two numbers.
+        - If no solution is found, return an empty list.
+
         Complexity:
         - Time: O(n), where n is the number of elements in `nums`. The
         function iterates through the list only once.
         - Space: O(n), for the dictionary used to store the number indices.
+
+        Raises:
+        - This function assumes that there is exactly one solution, and it
+        does not handle the case where no solution exists.
         """
         # Create a dictionary to store the numbers and their indices
         num_dict: Dict[int, int] = {}
