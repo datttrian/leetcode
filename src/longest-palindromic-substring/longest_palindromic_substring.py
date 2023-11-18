5085d82a52ea396a40fec219b39369419fff6ebd
datttrian <datttrian@gmail.com>
2023-11-18 16:49:50 +0000

    Sat, Nov 18, 2023, 4:49 PM +00:00

--- src/longest-palindromic-substring/longest_palindromic_substring.py
+++ src/longest-palindromic-substring/longest_palindromic_substring.py
@@ -1,29 +1,17 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         """
-        Find the longest palindromic substring in the given string.
+        Find the longest palindrome in a given string.
 
-        This algorithm, using the Expand Around Center technique, considers
-        each index in the string as the middle of a potential palindrome
-        and expands outwards checking for palindrome conditions. It expands
-        around each character (for odd-length palindromes) and each pair of
-        consecutive characters (for even-length palindromes), updating the
-        longest palindrome found.
-
-
-        Args:
-            s (str): The input string to search for the palindrome.
+        Parameters:
+        - s (str): The input string for which the longest palindrome needs to
+        be found.
 
         Returns:
-            str: The longest palindromic substring in the input string.
+        - str: The longest palindrome found in the input string.
 
-        Complexity:
-            Time: O(n^2), where n is the length of the input string. This time
-            complexity arises from the fact that for each of the n characters,
-            the expansion could potentially go through the rest of the string.
-
-            Space: O(1), as the space used does not depend on the size of the
-            input string and only a constant amount of extra space is used.
+        Raises:
+        - None
         """
         # String to hold the longest palindrome found
         res = ''
