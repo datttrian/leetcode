69e79a7178b6bf47d435f075b0887c6bec563d59
datttrian <datttrian@gmail.com>
2023-11-18 17:25:03 +0000

    Sat, Nov 18, 2023, 5:25 PM +00:00

---
+++ src/divide-two-integers/test_divide_two_integers.py
@@ -0,0 +1,21 @@
+import pytest
+from divide_two_integers import Solution
+
+
+@pytest.mark.parametrize(
+    ('dividend', 'divisor', 'expected_result'),
+    [
+        (10, 3, 3),
+        (7, -3, -2),
+        (-2147483648, -1, 2147483647),
+        (0, 1, 0),
+        (1, 1, 1),
+        (-2147483648, 2, -1073741824),
+        (2147483647, 1, 2147483647),
+        (-2147483648, -3, 715827882),
+    ],
+)
+def test_divide(dividend: int, divisor: int, expected_result: int):
+    solution = Solution()
+    result = solution.divide(dividend, divisor)
+    assert result == expected_result
