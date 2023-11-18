c1518804b276db03ef19147cee68b44473920937
datttrian <datttrian@gmail.com>
2023-11-18 17:15:21 +0000

    Sat, Nov 18, 2023, 5:15 PM +00:00

--- src/find-the-index-of-the-first-occurrence-in-a-string/test_find_the_index_of_the_first_occurrence_in_a_string.py
+++ src/find-the-index-of-the-first-occurrence-in-a-string/test_find_the_index_of_the_first_occurrence_in_a_string.py
@@ -1,26 +1,20 @@
-from typing import List
 import pytest
-from remove_element import Solution
+from find_the_index_of_the_first_occurrence_in_a_string import Solution
 
 
 @pytest.mark.parametrize(
-    ('nums', 'val', 'expected_result', 'expected_nums'),
+    ('haystack', 'needle', 'expected_result'),
     [
-        ([], 1, 0, []),
-        ([1, 2, 3, 4], 5, 4, [1, 2, 3, 4]),
-        ([3, 2, 2, 3], 3, 2, [2, 2]),
-        ([0, 1, 2, 2, 3, 0, 4, 2], 2, 5, [0, 1, 3, 0, 4]),
-        ([1, 1, 1, 1], 1, 0, []),
-        ([7, 7, 7, 7], 7, 0, []),
+        ('', '', 0),
+        ('hello', '', 0),
+        ('hello', 'll', 2),
+        ('aaaaa', 'bba', -1),
+        ('abcdef', 'ef', 4),
+        ('abc', 'abcd', -1),
+        ('abc', 'abc', 0),
     ],
 )
-def test_removeElement(
-    nums: List[int],
-    val: int,
-    expected_result: int,
-    expected_nums: List[int],
-):
+def test_strStr(haystack: str, needle: str, expected_result: int):
     solution = Solution()
-    result = solution.removeElement(nums, val)
+    result = solution.strStr(haystack, needle)
     assert result == expected_result
-    assert nums[:result] == expected_nums
