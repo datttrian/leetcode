70188c1903956433dfc7944a8f7683ab4091c783
datttrian <datttrian@gmail.com>
2023-11-18 17:25:43 +0000

    Sat, Nov 18, 2023, 5:25 PM +00:00

--- src/divide-two-integers/test_divide_two_integers.py
+++ src/divide-two-integers/test_divide_two_integers.py
@@ -10,6 +10,9 @@ from divide_two_integers import Solution
         (-2147483648, -1, 2147483647),
         (0, 1, 0),
         (1, 1, 1),
+        (-2147483648, 2, -1073741824),
+        # (2147483647, 1, 2147483647),
+        (-2147483648, -3, 715827882),
     ],
 )
 def test_divide(dividend: int, divisor: int, expected_result: int):
