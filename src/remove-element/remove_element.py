f76ec90153ebdd9759f4dc294e2761ec7d46e3b9
datttrian <datttrian@gmail.com>
2023-11-18 17:07:37 +0000

    Sat, Nov 18, 2023, 5:07 PM +00:00

--- src/remove-element/remove_element.py
+++ src/remove-element/remove_element.py
@@ -3,15 +3,10 @@ from typing import List
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        # Initialize the pointer
         i = 0
 
-        # Iterate through the array using enumerate
         for i, num in enumerate(nums):
-            # If the current element is not equal to val
             if num != val:
-                # Swap the current element with the element at the i-th
-                # position
                 nums[i] = num
                 i += 1
 
