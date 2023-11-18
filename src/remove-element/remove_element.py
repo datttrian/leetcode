34b40c70b4f3f87418de0dee565eeb5b0083cfdc
datttrian <datttrian@gmail.com>
2023-11-18 17:11:48 +0000

    Sat, Nov 18, 2023, 5:11 PM +00:00

--- src/remove-element/remove_element.py
+++ src/remove-element/remove_element.py
@@ -5,7 +5,7 @@ class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         i = 0
 
-        for i, num in enumerate(nums):
+        for _, num in enumerate(nums):
             if num != val:
                 nums[i] = num
                 i += 1
