5d12729887377ceedb661031451e254f7f0987fb
datttrian <datttrian@gmail.com>
2023-11-20 11:07:02 +0000

    Mon, Nov 20, 2023, 11:07 AM +00:00

--- src/count-and-say/count_and_say.py
+++ src/count-and-say/count_and_say.py
@@ -7,6 +7,6 @@ class Solution:
         for _ in range(n - 1):
             s = ''.join(
                 str(len(group)) + digit
-                for group, digit in re.findall(r'((.)\2*)', s)  # type: ignore
-            )  # type: ignore
+                for group, digit in re.findall(r'((.)\2*)', s)
+            )
         return s
