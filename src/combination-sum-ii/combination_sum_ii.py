11fc08642019c4a45cdbb181b44169142b216f55
datttrian <datttrian@gmail.com>
2023-11-20 11:24:27 +0000

    Mon, Nov 20, 2023, 11:24 AM +00:00

--- src/combination-sum-ii/combination_sum_ii.py
+++ src/combination-sum-ii/combination_sum_ii.py
@@ -13,16 +13,13 @@ class Solution:
                 return
 
             for i in range(start, len(candidates)):
-                # Skip duplicates to avoid duplicate combinations
                 if i > start and candidates[i] == candidates[i - 1]:
                     continue
 
-                # Skip elements if they exceed the remaining target
                 if candidates[i] > target:
                     continue
 
                 path.append(candidates[i])
-                # Recursively find combinations with the remaining target
                 backtrack(i + 1, target - candidates[i], path)
                 path.pop()
 
---
+++ src/combination-sum-ii/test_combination_sum_ii.py
@@ -0,0 +1,26 @@
+from typing import List
+import pytest
+from combination_sum_ii import Solution
+
+
+@pytest.mark.parametrize(
+    ('candidates', 'target', 'expected_output'),
+    [
+        ([10, 1, 2, 7, 6, 1, 5], 8, [[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]]),
+        ([2, 5, 2, 1, 2], 5, [[1, 2, 2], [5]]),
+        # Add more test cases as needed
+    ],
+)
+def test_combinationSum2(
+    candidates: List[int],
+    target: int,
+    expected_output: List[List[int]],
+):
+    solution = Solution()
+    result = solution.combinationSum2(candidates, target)
+    assert result == expected_output
+
+
+# Run the tests
+if __name__ == '__main__':
+    pytest.main()
