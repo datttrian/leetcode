e0ef478326421a2e8b6173e4133fde7849981954
datttrian <datttrian@gmail.com>
2023-11-21 10:32:39 +0000

    Tue, Nov 21, 2023, 10:32 AM +00:00

---
+++ src/maximum-subarray/test_maximum_subarray.py
@@ -0,0 +1,22 @@
+from typing import List
+import pytest
+from maximum_subarray import Solution
+
+
+@pytest.mark.parametrize(
+    ('nums', 'expected'),
+    [
+        ([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
+        ([1], 1),
+        ([5, 4, -1, 7, 8], 23),
+        ([-2, -1, -3, -4, -1, -2, -1, -5, -4], -1),
+        ([], 0),
+        ([-1, -2, -3, -4, -5], -1),
+        ([10, -2, 5, 1, -1, 7, -2], 20),
+        ([-1, 0, -2], 0),
+    ],
+)
+def test_maxSubArray(nums: List[int], expected: int):
+    solution = Solution()
+    result = solution.maxSubArray(nums)
+    assert result == expected
