cb0eab292c497ee24287615aabcc73785b8fec2b
datttrian <datttrian@gmail.com>
2023-11-23 09:48:40 +0000

    Thu, Nov 23, 2023, 9:48 AM +00:00

--- src/permutations-ii/permutations_ii.py
+++ src/permutations-ii/permutations_ii.py
@@ -1,31 +1,19 @@
-from typing import List
-
-
 class Solution:
-    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+    def permuteUnique(self, nums: list[int]) -> list[list[int]]:
         def backtrack(start: int):
-            if start == len(nums):
-                result.append(
-                    nums.copy(),
-                )  # Make a copy of the current permutation
+            if start == len(nums) - 1:
+                result.append(nums[:])
                 return
-
+            unique_set: set[int] = set()
             for i in range(start, len(nums)):
-                # Skip duplicates to avoid duplicate permutations
-                if i > start and nums[i] == nums[i - 1]:
+                if nums[i] in unique_set:
                     continue
-
-                # Swap the current element with the element at the current
-                # position
+                unique_set.add(nums[i])
                 nums[start], nums[i] = nums[i], nums[start]
-
-                # Recursively generate permutations for the remaining elements
                 backtrack(start + 1)
-
-                # Undo the swap to backtrack and explore other possibilities
                 nums[start], nums[i] = nums[i], nums[start]
 
-        result: List[List[int]] = []
-        nums.sort()  # Sort the input array to handle duplicates
+        result: list[list[int]] = []
+        nums.sort()
         backtrack(0)
         return result
