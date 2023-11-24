2848b929a1a59782deba3015b0672538e7739359
datttrian <datttrian@gmail.com>
2023-11-24 10:27:01 +0000

    Fri, Nov 24, 2023, 10:27 AM +00:00

---
+++ leetcode/validate-binary-search-tree/validate_binary_search_tree.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(
+        self,
+        val: int = 0,
+        left: Optional['TreeNode'] = None,
+        right: Optional['TreeNode'] = None,
+    ):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        output: list[int] = []
+        self.inorder(root, output)
+
+        for i in range(1, len(output)):
+            if output[i - 1] >= output[i]:
+                return False
+
+        return True
+
+    # Time complexity of inorder traversal is O(n)
+    # Fun fact: Inorder traversal leads to a sorted array if it is
+    # a Valid Binary Search Tree.
+    def inorder(self, root: Optional[TreeNode], output: list[int]) -> None:
+        if root is None:
+            return
+
+        self.inorder(root.left, output)
+        output.append(root.val)
+        self.inorder(root.right, output)
