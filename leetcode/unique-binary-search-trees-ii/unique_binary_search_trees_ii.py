2c9f75de6fe4c20f7a7e8c1478a18665f3c08b8f
datttrian <datttrian@gmail.com>
2023-11-24 09:40:50 +0000

    Fri, Nov 24, 2023, 9:40 AM +00:00

--- leetcode/unique-binary-search-trees-ii/unique_binary_search_trees_ii.py
+++ leetcode/unique-binary-search-trees-ii/unique_binary_search_trees_ii.py
@@ -6,8 +6,8 @@ class TreeNode:
     def __init__(
         self,
         val: int = 0,
-        left: Union['TreeNode', None] = None,
-        right: Union['TreeNode', None] = None,
+        left: 'typing.Type[TreeNode]' = None,
+        right: 'typing.Type[TreeNode]' = None,
     ):
         self.val = val
         self.left = left
