1c9b003147a6ba7f5716617f7b3b749d1f476051
datttrian <datttrian@gmail.com>
2023-11-25 07:54:05 +0000

    Sat, Nov 25, 2023, 7:54 AM +00:00

--- leetcode/valid-number/valid_number.py
+++ leetcode/valid-number/valid_number.py
@@ -1,7 +1,24 @@
-import re
-
-
 class Solution:
     def isNumber(self, s: str) -> bool:
-        pattern = re.compile(r'^[+-]?(\d+\.\d*|\.\d+|\d+)([eE][+-]?\d+)?$')
-        return bool(pattern.match(s))
+        num, exp, sign, dec = False, False, False, False
+        for c in s:
+            if c >= '0' and c <= '9':
+                num = True
+            elif c == 'e' or c == 'E':
+                if exp or not num:
+                    return False
+                else:
+                    exp, num, sign, dec = True, False, False, False
+            elif c == '+' or c == '-':
+                if sign or num or dec:
+                    return False
+                else:
+                    sign = True
+            elif c == '.':
+                if dec or exp:
+                    return False
+                else:
+                    dec = True
+            else:
+                return False
+        return num
