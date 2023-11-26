dd6c1232fd18dbee64a897e0c5565e46e867b35a
datttrian <datttrian@gmail.com>
2023-11-26 09:58:52 +0000

    Sun, Nov 26, 2023, 9:58 AM +00:00

--- leetcode/search-insert-position/search_insert_position.py
+++ leetcode/search-insert-position/search_insert_position.py
@@ -1,8 +1,5 @@
-from typing import List
-
-
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
+    def searchInsert(self, nums: list[int], target: int) -> int:
         left, right = 0, len(nums) - 1
 
         while left <= right:
@@ -10,7 +7,7 @@ class Solution:
 
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
+            if nums[mid] < target:
                 left = mid + 1
             else:
                 right = mid - 1
