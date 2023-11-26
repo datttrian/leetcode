2f8b3d36bf2fabe20eeedde0295b5bcc042651d4
datttrian <datttrian@gmail.com>
2023-11-26 12:01:49 +0000

    Sun, Nov 26, 2023, 12:01 PM +00:00

--- leetcode/roman-to-integer/roman_to_integer.py
+++ leetcode/roman-to-integer/roman_to_integer.py
@@ -1,30 +1,15 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        """
-        Convert a Roman numeral string to an integer.
-
-        Parameters:
-        - s (str): The input Roman numeral string. It should be a non-empty
-        string containing valid Roman numeral characters. The length of the
-        string should be between 1 and 15, inclusive.
-
-        Returns:
-        - int: The integer representation of the input Roman numeral string.
-
-        Raises:
-        The Roman numeral input is assumed to be valid, and no error handling
-        is implemented for invalid inputs.
-        """
-
-        # Dictionary mapping Roman numerals to their corresponding integer values
+        # Dictionary mapping Roman numerals to their corresponding integer
+        # values
         roman_dict: dict[str, int] = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000,
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
         }
 
         # Initialize result and keep track of the previous value
@@ -35,7 +20,8 @@ class Solution:
         for char in reversed(s):
             current_value: int = roman_dict[char]
 
-            # If the current value is less than the previous value, subtract it; otherwise, add it
+            # If the current value is less than the previous value, subtract
+            # it; otherwise, add it
             if current_value < prev_value:
                 result -= current_value
             else:
