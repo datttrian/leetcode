a3972526facf58af755cc9f70045b9a1af142500
datttrian <datttrian@gmail.com>
2023-11-27 04:04:34 +0000

    Mon, Nov 27, 2023, 4:04 AM +00:00

--- leetcode/roman-to-integer/roman_to_integer.py
+++ leetcode/roman-to-integer/roman_to_integer.py
@@ -1,8 +1,6 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        # Dictionary mapping Roman numerals to their corresponding integer
-        # values
-        roman_dict: dict[str, int] = {
+        roman = {
             "I": 1,
             "V": 5,
             "X": 10,
@@ -11,24 +9,10 @@ class Solution:
             "D": 500,
             "M": 1000,
         }
-
-        # Initialize result and keep track of the previous value
-        result: int = 0
-        prev_value: int = 0
-
-        # Iterate through the string in reverse order
-        for char in reversed(s):
-            current_value: int = roman_dict[char]
-
-            # If the current value is less than the previous value, subtract
-            # it; otherwise, add it
-            if current_value < prev_value:
-                result -= current_value
+        res = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+                res -= roman[s[i]]
             else:
-                result += current_value
-
-            # Update the previous value for the next iteration
-            prev_value = current_value
-
-        # Return the final result
-        return result
+                res += roman[s[i]]
+        return res
--- style.sh
+++ style.sh
@@ -1,5 +1,5 @@
 set -xe
-poetry run isort leetcode --check-only
-poetry run flake8 leetcode --show-source
-poetry run pylint leetcode
-poetry run mypy leetcode
+poetry run isort leetcode/roman-to-integer/roman_to_integer.py --check-only
+poetry run flake8 leetcode/roman-to-integer/roman_to_integer.py --show-source
+poetry run pylint leetcode/roman-to-integer/roman_to_integer.py
+poetry run mypy leetcode/roman-to-integer/roman_to_integer.py
