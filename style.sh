33724d439c87e579107e61dfc10c5d77d91f18f3
datttrian <datttrian@gmail.com>
2023-11-27 09:38:22 +0000

    Mon, Nov 27, 2023, 9:38 AM +00:00

--- style.sh
+++ style.sh
@@ -1,6 +1,6 @@
 set -xe
 
-poetry run isort leetcode/roman-to-integer/*.py --check-only
-poetry run flake8 leetcode/roman-to-integer/*.py --show-source
-poetry run pylint leetcode/roman-to-integer/*.py
-poetry run mypy leetcode/roman-to-integer/*.py
+poetry run isort leetcode --check-only
+poetry run flake8 leetcode --show-source
+poetry run pylint leetcode
+poetry run mypy leetcode
