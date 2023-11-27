59f293c389f5b57ec25fd237aea6974e7272bb1c
datttrian <datttrian@gmail.com>
2023-11-27 09:56:25 +0000

    Mon, Nov 27, 2023, 9:56 AM +00:00

--- leetcode/integer-to-roman/integer_to_roman.py
+++ leetcode/integer-to-roman/integer_to_roman.py
@@ -1,27 +1,24 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        symbols = [
-            "M",
-            "CM",
-            "D",
-            "CD",
-            "C",
-            "XC",
-            "L",
-            "XL",
-            "X",
-            "IX",
-            "V",
-            "IV",
-            "I",
+        symList = [
+            ("I", 1),
+            ("IV", 4),
+            ("V", 5),
+            ("IX", 9),
+            ("X", 10),
+            ("XL", 40),
+            ("L", 50),
+            ("XC", 90),
+            ("C", 100),
+            ("CD", 400),
+            ("D", 500),
+            ("CM", 900),
+            ("M", 1000),
         ]
-        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-
-        result = ""
-
-        for symbol, value in zip(symbols, values):
-            while num >= value:
-                result += symbol
-                num -= value
-
-        return result
+        res = ""
+        for sym, val in reversed(symList):
+            if num // val:
+                count = num // val
+                res += sym * count
+                num = num % val
+        return res
--- leetcode/integer-to-roman/test_integer_to_roman.py
+++ leetcode/integer-to-roman/test_integer_to_roman.py
@@ -1,21 +1,19 @@
 import pytest
-from integer_to_roman import (  # Assuming your solution is in a module named solution_module
-    Solution,
-)
+from integer_to_roman import Solution
 
 
 @pytest.mark.parametrize(
-    ('num', 'expected_roman'),
+    ["num", "expected_roman"],
     [
-        (1, 'I'),
-        (4, 'IV'),
-        (9, 'IX'),
-        (58, 'LVIII'),
-        (1994, 'MCMXCIV'),
+        [1, "I"],
+        [4, "IV"],
+        [9, "IX"],
+        [58, "LVIII"],
+        [1994, "MCMXCIV"],
         # Add more test cases as needed
     ],
 )
-def test_intToRoman(num: int, expected_roman: str):
+def test_intToRoman(num: int, expected_roman: str) -> None:
     solution = Solution()
     result = solution.intToRoman(num)
     assert result == expected_roman
