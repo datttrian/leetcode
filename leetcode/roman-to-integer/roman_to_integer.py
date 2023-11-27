4d1ce6d014eb7492a6fc9bc6ca42edb9d33be68d
datttrian <datttrian@gmail.com>
2023-11-27 19:46:43 +0000

    Mon, Nov 27, 2023, 7:46 PM +00:00

--- leetcode/roman-to-integer/roman_to_integer.py
+++ leetcode/roman-to-integer/roman_to_integer.py
@@ -1,5 +1,25 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        """Convert a Roman numeral string to an integer.
+
+        The function takes a Roman numeral string as input and returns its
+        corresponding integer value. Roman numerals are represented by
+        combinations of the symbols 'I', 'V', 'X', 'L', 'C', 'D', and 'M',
+        where each symbol has a specific numeric value. The numeral string is
+        processed from right to left, and the values are accumulated based on
+        the rules of Roman numeral representation.
+
+        Args:
+            s (str): A Roman numeral string consisting of uppercase letters.
+
+        Returns:
+            int: The integer value corresponding to the input Roman numeral
+            string.
+
+        Raises:
+            None: It is guaranteed that s is a valid roman numeral in the
+            range [1, 3999].
+        """
         roman_dict = {
             "I": 1,
             "V": 5,
