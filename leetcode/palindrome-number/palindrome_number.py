dc028098f598b53ee0e9770cde19000f69eae6bd
datttrian <datttrian@gmail.com>
2023-11-28 12:27:05 +0000

    Tue, Nov 28, 2023, 12:27 PM +00:00

--- leetcode/palindrome-number/palindrome_number.py
+++ leetcode/palindrome-number/palindrome_number.py
@@ -1,10 +1,19 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        # Check if the number is negative or ends with zero except 0
         if x < 0 or (x % 10 == 0 and x != 0):
             return False
+
+        # Variable to store the reversed number
         reversed_num = 0
+
+        # Save the original number for comparison
         original_num = x
+
+        # Reverse the digits of the number
         while x > 0:
             x, digit = divmod(x, 10)
             reversed_num = reversed_num * 10 + digit
+
+        # Check if the reversed number is equal to the original number
         return reversed_num == original_num
