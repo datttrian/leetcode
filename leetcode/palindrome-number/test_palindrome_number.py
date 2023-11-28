3c64e3ea7f04fe096090e53acec17f09090e5c59
datttrian <datttrian@gmail.com>
2023-11-28 12:34:23 +0000

    Tue, Nov 28, 2023, 12:34 PM +00:00

--- leetcode/palindrome-number/test_palindrome_number.py
+++ leetcode/palindrome-number/test_palindrome_number.py
@@ -3,16 +3,16 @@ from palindrome_number import Solution
 
 
 @pytest.mark.parametrize(
-    ('input_num', 'expected'),
+    ["input_num", "expected"],
     [
-        (121, True),
-        (-121, False),
-        (10, False),
-        (0, True),
-        (12321, True),
-        (123456, False),
+        [121, True],
+        [-121, False],
+        [10, False],
+        [0, True],
+        [12321, True],
+        [123456, False],
     ],
 )
-def test_isPalindrome(input_num: int, expected: bool):
+def test_isPalindrome(input_num: int, expected: bool) -> None:
     solution = Solution()
     assert solution.isPalindrome(input_num) == expected
