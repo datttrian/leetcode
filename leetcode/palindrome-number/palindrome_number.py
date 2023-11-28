6e505452c48abb246181750a3cf2aa4e1a903aea
datttrian <datttrian@gmail.com>
2023-11-28 12:28:57 +0000

    Tue, Nov 28, 2023, 12:28 PM +00:00

--- leetcode/palindrome-number/palindrome_number.py
+++ leetcode/palindrome-number/palindrome_number.py
@@ -1,5 +1,19 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        """Check if an integer is a palindrome.
+
+        The determination is achieved by reversing its digits and comparing
+        the reversed number with the original.
+
+        Args:
+            x (int): The integer to be checked for palindromicity.
+
+        Returns:
+            bool: True if the integer is a palindrome, False otherwise.
+
+        Raises:
+            None: It is guaranteed that the input is an integer.
+        """
         # Check if the number is negative or ends with zero except 0
         if x < 0 or (x % 10 == 0 and x != 0):
             return False
