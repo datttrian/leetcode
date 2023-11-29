1064c894d8349e47fc7c4a2aeea8b097a4ce1dcd
datttrian <datttrian@gmail.com>
2023-11-29 18:36:39 +0000

    Wed, Nov 29, 2023, 6:36 PM +00:00

--- leetcode/remove-duplicates-from-sorted-array/remove_duplicates_from_sorted_array.py
+++ leetcode/remove-duplicates-from-sorted-array/remove_duplicates_from_sorted_array.py
@@ -3,16 +3,8 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        """
-        Remove duplicates from a sorted list in-place and returns the new
-        length.
+        """Remove duplicates from a sorted list in-place.
 
-        Summary:
-        Given a sorted list, this function modifies the list in-place to
-        remove duplicate elements, and returns the length of the modified list
-        without duplicates.
-
-        Description:
         The function iterates through the sorted list and maintains a pointer
         'k' representing the index where the next unique element should be
         placed. If the current element is different from the previous one, it
@@ -20,17 +12,7 @@ class Solution:
         iteration, the modified list is truncated to the length 'k', and 'k'
         is returned.
 
-        Algorithm:
-        1. Initialize a pointer 'k' to 1 (since the first element is always
-        unique).
-        2. Iterate through the list starting from index 1.
-        3. If the current element is different from the previous one, place it
-        at index 'k' and increment 'k'.
-        4. After the iteration, truncate the list to the length 'k'.
-        5. Return the value of 'k', which represents the new length of the
-        list without duplicates.
-
-        Parameters:
+        Args:
         - nums (List[int]): A sorted list of integers with possible duplicate
         elements.
 
@@ -39,11 +21,6 @@ class Solution:
 
         Raises:
         - No explicit exceptions are raised.
-
-        Complexity:
-        - Time: O(n), where n is the length of the input list 'nums'.
-        - Space: O(1), as the modification is done in-place, and the algorithm
-        uses constant space.
         """
         # Check if the input list is empty
         if not nums:
