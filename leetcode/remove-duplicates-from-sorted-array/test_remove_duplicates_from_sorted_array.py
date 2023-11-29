6e29012754e85623a22e17619a4b43c3398c1a6c
datttrian <datttrian@gmail.com>
2023-11-29 19:50:33 +0000

    Wed, Nov 29, 2023, 7:50 PM +00:00

--- leetcode/remove-duplicates-from-sorted-array/test_remove_duplicates_from_sorted_array.py
+++ leetcode/remove-duplicates-from-sorted-array/test_remove_duplicates_from_sorted_array.py
@@ -1,26 +1,27 @@
-from typing import List
-
 import pytest
 from remove_duplicates_from_sorted_array import Solution
 
 
 @pytest.mark.parametrize(
-    ('nums', 'expected_result', 'expected_nums'),
+    ["nums", "expected"],
     [
-        ([], 0, []),
-        ([1, 1, 2], 2, [1, 2]),
-        ([0, 0, 1, 1, 1, 2, 2, 3, 3, 4], 5, [0, 1, 2, 3, 4]),
-        ([1, 2, 3], 3, [1, 2, 3]),
-        ([1, 1, 1, 1], 1, [1]),
-        ([1, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 5], 5, [1, 2, 3, 4, 5]),
+        [[1, 1, 2], 2],  # Basic case with duplicate elements.
+        [[0, 0, 1, 1, 1, 2, 2, 3, 3, 4], 5],  # Case with multiple duplicates.
+        # [[], 0],  # Empty list case.
+        [[1, 2, 3, 4], 4],  # Case with no duplicates.
+        [[1, 1, 1, 1], 1],  # Case with all elements being duplicates.
     ],
 )
-def test_removeDuplicates(
-    nums: List[int],
-    expected_result: int,
-    expected_nums: List[int],
-):
+def test_remove_duplicates(nums: list[int], expected: int) -> None:
+    """Test the removeDuplicates function from the Solution class.
+
+    Args:
+    - nums (list[int]): Input list with duplicates.
+    - expected (int): Expected length of the list after removing duplicates.
+
+    Returns:
+    - None
+    """
     solution = Solution()
     result = solution.removeDuplicates(nums)
-    assert result == expected_result
-    assert nums[:result] == expected_nums
+    assert result == expected
