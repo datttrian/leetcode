857467c1bfc5df47448e5ee00741d8ddc7d3fc1a
datttrian <datttrian@gmail.com>
2023-11-29 19:49:09 +0000

    Wed, Nov 29, 2023, 7:49 PM +00:00

--- leetcode/remove-duplicates-from-sorted-array/remove_duplicates_from_sorted_array.py
+++ leetcode/remove-duplicates-from-sorted-array/remove_duplicates_from_sorted_array.py
@@ -1,41 +1,9 @@
-from typing import List
-
-
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        """Remove duplicates from a sorted list in-place.
+    def removeDuplicates(self, nums: list[int]) -> int:
+        L = 1
 
-        The function iterates through the sorted list and maintains a pointer
-        'k' representing the index where the next unique element should be
-        placed. If the current element is different from the previous one, it
-        is placed at index 'k', and 'k' is incremented. At the end of the
-        iteration, the modified list is truncated to the length 'k', and 'k'
-        is returned.
-
-        Args:
-        - nums (List[int]): A sorted list of integers with possible duplicate
-        elements.
-
-        Returns:
-        - int: The length of the modified list without duplicates.
-
-        Raises:
-        - No explicit exceptions are raised.
-        """
-        # Check if the input list is empty
-        if not nums:
-            return 0
-
-        # Initialize the pointer 'k' to 1 (the first element is always unique)
-        k = 1
-
-        # Iterate through the list starting from index 1
-        for i in range(1, len(nums)):
-            # If the current element is different from the previous one
-            if nums[i] != nums[i - 1]:
-                # Place it at index 'k' and increment 'k'
-                nums[k] = nums[i]
-                k += 1
-
-        # Truncate the list to the length 'k' and return 'k'
-        return k
+        for R in range(1, len(nums)):
+            if nums[R] != nums[R - 1]:
+                nums[L] = nums[R]
+                L += 1
+        return L
