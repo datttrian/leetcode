cb24c9de176240d7680f3942b7a613e7a9fb2bc0
datttrian <datttrian@gmail.com>
2023-11-30 20:01:12 +0000

    Thu, Nov 30, 2023, 8:01 PM +00:00

---
+++ leetcode/palindrome-number/math_geometry/__init__.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        """Check if an integer is a palindrome.
+
+        The determination is achieved by reversing the input number digits and
+        comparing the reversed number with the original.
+
+        Args:
+            x (int): The integer to be checked for palindromicity.
+
+        Returns:
+            bool: True if the integer is a palindrome, False otherwise.
+
+        Raises:
+            None: It is guaranteed that the input is an integer.
+        """
+        # Check if the number is negative or ends with zero except 0
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        # Variable to store the reversed number
+        reversed_num = 0
+
+        # Variable to save the original number for comparison
+        original_num = x
+
+        # Reverse the digits of the number
+        while x > 0:
+            x, digit = divmod(x, 10)
+            reversed_num = reversed_num * 10 + digit
+
+        # Check if the reversed number is equal to the original number
+        return reversed_num == original_num
--- leetcode/palindrome-number/test_palindrome_number.py
+++ leetcode/palindrome-number/test_palindrome_number.py
@@ -1,26 +1,48 @@
-import pytest
-from array_hashing.palindrome_number import Solution
+# import pytest
+# from array_hashing import Solution as ArraySolution
+# from linked_list import Solution as LinkedListSolution
+# from stack import Solution as StackSolution
+# from math_geometry import Solution as MathSolution
+# from two_pointers import Solution as TwoPointers
 
 
-@pytest.mark.parametrize(
-    ["input_num", "expected"],
-    [
-        [121, True],
-        [-121, False],
-        [10, False],
-        [0, True],
-        [12321, True],
-        [123456, False],
-    ],
-)
-def test_is_palindrome(input_num: int, expected: bool) -> None:
-    """Test the isPalindrome function from the Solution class.
+# @pytest.fixture(
+#     params=[
+#         ArraySolution,
+#         StackSolution,
+#         LinkedListSolution,
+#         MathSolution,
+#         TwoPointers,
+#     ],
+# )
+# def solution(request):
+#     return request.param()
 
-    Args:
-    - x (int): Input number.
 
-    Returns:
-    - None
-    """
-    solution = Solution()
-    assert solution.isPalindrome(input_num) == expected
+# @pytest.mark.parametrize(
+#     ["input_num", "expected"],
+#     [
+#         [121, True],
+#         [-121, False],
+#         [10, False],
+#         [0, True],
+#         [12321, True],
+#         [123456, False],
+#     ],
+# )
+# def test_is_palindrome(
+#     solution: object,
+#     input_num: int,
+#     expected: bool,
+# ) -> None:
+#     """Test the isPalindrome function from the given Solution class.
+
+#     Args:
+#     - solution (object): Instance of the Solution class.
+#     - input_num (int): Input number.
+#     - expected (bool): Expected result.
+
+#     Returns:
+#     - None
+#     """
+#     assert solution.isPalindrome(input_num) == expected
