29feceddaa11d379ccc7d1488e9624932133e0b8
datttrian <datttrian@gmail.com>
2023-12-01 17:43:36 +0000

    Fri, Dec 1, 2023, 5:43 PM +00:00

--- leetcode/palindrome-number/palindrome_number.py
+++ leetcode/palindrome-number/palindrome_number.py
@@ -1,33 +1,13 @@
-from typing import Optional
-
-
-class ListNode:
-    def __init__(
-        self,
-        val: int = 0,
-        next_node: "Optional[ListNode]" = None,
-    ) -> None:
-        self.value: int = val
-        self.next: Optional[ListNode] = next_node
-
-
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0 or (x % 10 == 0 and x != 0):
             return False
 
-        head = None
-        original_x = x
+        reversed_num = 0
+        original_num = x
+
         while x > 0:
-            digit = x % 10
-            x //= 10
-            head = ListNode(digit, head)
+            x, digit = divmod(x, 10)
+            reversed_num = reversed_num * 10 + digit
 
-        current = head
-        while original_x > 0 and current:
-            if current.value != original_x % 10:
-                return False
-            current = current.next
-            original_x //= 10
-
-        return not current
+        return reversed_num == original_num
