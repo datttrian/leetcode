6ec46df7d058c569a524fe609bc7c8f4329ea66a
datttrian <datttrian@gmail.com>
2023-12-01 16:10:13 +0000

    Fri, Dec 1, 2023, 4:10 PM +00:00

--- leetcode/remove-duplicates-from-sorted-array/remove_duplicates_from_sorted_array.py
+++ leetcode/remove-duplicates-from-sorted-array/remove_duplicates_from_sorted_array.py
@@ -1,5 +1,32 @@
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
+        """Remove duplicates from a list of integers and return the length of
+        the updated array.
+
+        The `removeDuplicates` method takes a list of integers as input,
+        removes any duplicate elements, and updates the original list with the
+        unique elements in sorted order. The method then returns the length of
+        the updated array.
+
+        Args:
+        - nums (list[int]): The input list of integers with possible
+        duplicates.
+
+        Returns:
+        - int: The length of the updated array after removing duplicates.
+
+        Raises:
+        - None
+
+        Example:
+        ```python
+        solution = Solution()
+        nums = [1, 2, 2, 3, 4, 4, 5]
+        length = solution.removeDuplicates(nums)
+        print(length)  # Output: 5
+        print(nums)    # Output: [1, 2, 3, 4, 5, 4, 5]
+        ```
+        """
         # Convert the list to a set to remove duplicates
         unique_nums = set(nums)
 
