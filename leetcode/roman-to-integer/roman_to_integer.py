863301516e8ba04fa58d9d9333ac19ff4a55149f
ttran375 <137940945+ttran375@users.noreply.github.com>
2023-12-21 02:09:38 +0000

    Thu, Dec 21, 2023, 2:09 AM +00:00

--- leetcode/roman-to-integer/roman_to_integer.py
+++ leetcode/roman-to-integer/roman_to_integer.py
@@ -1,6 +1,7 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        """Convert a Roman numeral to an integer.
+        """
+        Convert a Roman numeral to an integer.
 
         Parameters:
         s (str): A string representing a Roman numeral.
@@ -8,6 +9,8 @@ class Solution:
         Returns:
         int: The integer representation of the Roman numeral.
         """
+
+        # Define a dictionary to map numerals to integer
         roman_dict = {
             "I": 1,
             "V": 5,
@@ -17,17 +20,26 @@ class Solution:
             "D": 500,
             "M": 1000,
         }
+
+        # Initialize the result and the previous value
         result = 0
         prev_value = 0
 
+        # Iterate over the string in reverse order
         for char in reversed(s):
+            # Get the integer value of the current Roman numeral
             current_value = roman_dict[char]
 
+            # If the current value is less than the previous value
             if current_value < prev_value:
+                
                 result -= current_value
             else:
+                # Otherwise, add the current value to the result
                 result += current_value
 
+            # Update the previous value for the next iteration
             prev_value = current_value
 
+        # Return the final result
         return result
