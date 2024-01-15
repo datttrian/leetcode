8dd36136c9b45e80478416753b11cebf9bba827d
Dat Tran <datttrian@gmail.com>
2024-01-15 22:29:38 +0000

    Mon, Jan 15, 2024, 10:29 PM +00:00

--- leetcode/happy-number/happy_number.py
+++ leetcode/happy-number/happy_number.py
@@ -1,10 +1,10 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        def get_next(num: int) -> int:
+        def sum_squared_digit(num: int) -> int:
             return sum(int(digit) ** 2 for digit in str(num))
 
-        current, next_num = n, get_next(n)
+        current, next_num = n, sum_squared_digit(n)
         while next_num not in (1, current):
-            current = get_next(current)
-            next_num = get_next(get_next(next_num))
+            current = sum_squared_digit(current)
+            next_num = sum_squared_digit(sum_squared_digit(next_num))
         return next_num == 1
