f4fa64c3c6743acb57648bb48c74d7c1489d4299
Dat Tran <datttrian@gmail.com>
2024-01-15 20:10:22 +0000

    Mon, Jan 15, 2024, 8:10 PM +00:00

--- leetcode/happy-number/test_roman_to_integer.py
+++ leetcode/happy-number/test_roman_to_integer.py
@@ -1,41 +1,29 @@
 import pytest
-from roman_to_integer import Solution
+from recursive import Solution
 
 
 @pytest.mark.parametrize(
-    ["s", "expected"],
+    ["n", "expected"],
     [
-        ["III", 3],  # Basic case with a simple Roman numeral.
-        ["LVIII", 58],  # Complex case with a combination of Roman numerals.
-        ["I", 1],  # Minimal input case.
-        ["IX", 9],  # Case with subtraction (IX = 10 - 1).
-        [
-            "CDXLIV",
-            444,
-        ],  # Case with a combination of subtraction and addition.
-        ["MCMXCIV", 1994],  # Case with a mix of different symbols.
-        ["XC", 90],  # Case with a single subtraction (XC = 100 - 10).
-        ["XL", 40],  # Another case with a single subtraction (XL = 50 - 10).
-        ["CXL", 140],  # Case with multiple symbols and a subtraction.
-        # Largest possible Roman numeral (MMMCMXCIX = 1000 + 1000 + 1000 - 100
-        # + 100 - 10 + 9).
-        [
-            "MMMCMXCIX",
-            3999,
-        ],
-        ["", 0],  # Empty string case.
+        [19, True],  # Basic case with a happy number.
+        [2, False],  # Basic case with a non-happy number.
+        [1, True],  # Minimal input case.
+        [7, True],  # Another basic case with a happy number.
+        [1111111, True],  # Case with a large non-happy number.
+        [44, True],  # Case with a large happy number.
+        [0, False],  # Edge case with zero.
     ],
 )
-def test_roman_to_int(s: str, expected: int) -> None:
-    """Test the romanToInt function from the Solution class.
+def test_is_happy(n: int, expected: bool) -> None:
+    """Test the isHappy function from the Solution class.
 
     Args:
-    - s (str): Roman numeral input.
-    - expected (int): Expected integer output.
+    - n (int): Integer input.
+    - expected (bool): Expected boolean output.
 
     Returns:
     - None
     """
     solution = Solution()
-    result = solution.romanToInt(s)
+    result = solution.isHappy(n)
     assert result == expected
