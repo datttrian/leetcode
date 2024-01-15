b8124daeba4f3e32eedaac56acaee69c17f036a3
Dat Tran <datttrian@gmail.com>
2024-01-15 21:58:10 +0000

    Mon, Jan 15, 2024, 9:58 PM +00:00

--- leetcode/happy-number/pointers.py
+++ leetcode/happy-number/pointers.py
@@ -1,6 +1,24 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
+        """Determines whether a given number is a happy number.
+
+        Parameters:
+        - n (int): The input number to check for happiness.
+
+        Returns:
+        - bool: True if the number is a happy number, False otherwise.
+        """
+
         def get_next(num: int) -> int:
+            """Calculates the next number in the sequence for determining
+            happiness.
+
+            Parameters:
+            - num (int): The current number in the sequence.
+
+            Returns:
+            - int: The next number in the sequence.
+            """
             return sum(int(digit) ** 2 for digit in str(num))
 
         current, next_num = n, get_next(n)
