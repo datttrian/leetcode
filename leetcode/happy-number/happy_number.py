5d43486cfedfb0d5a7081ae26074708ed2e657c1
Dat Tran <datttrian@gmail.com>
2024-01-15 22:31:29 +0000

    Mon, Jan 15, 2024, 10:31 PM +00:00

--- leetcode/happy-number/happy_number.py
+++ leetcode/happy-number/happy_number.py
@@ -3,6 +3,13 @@ class Solution:
         def sum_squared_digit(num: int) -> int:
             return sum(int(digit) ** 2 for digit in str(num))
 
+        # def sum_squared_digit(num: int) -> int:
+        #     total = 0
+        #     while num > 0:
+        #         num, digit = divmod(num, 10)
+        #         total += digit**2
+        #     return total
+
         current, next_num = n, sum_squared_digit(n)
         while next_num not in (1, current):
             current = sum_squared_digit(current)
