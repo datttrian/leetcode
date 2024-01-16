27ba2fa18080d982d2a8e5a8cc21c3a80d7ffc83
Dat Tran <datttrian@gmail.com>
2024-01-16 23:42:12 +0000

    Tue, Jan 16, 2024, 11:42 PM +00:00

--- leetcode/multiply-strings/multiply_strings.py
+++ leetcode/multiply-strings/multiply_strings.py
@@ -1,19 +1,16 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        num1 = num1[::-1]
-        num2 = num2[::-1]
+        m, n = len(num1), len(num2)
+        pos = [0] * (m + n)
 
-        result: list[int] = [0] * (len(num1) + len(num2))
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                mul = int(num1[i]) * int(num2[j])
+                p1, p2 = i + j, i + j + 1
+                total_sum = mul + pos[p2]
 
-        for i, digit1 in enumerate(num1):
-            for j, digit2 in enumerate(num2):
-                result[i + j] += int(digit1) * int(digit2)
-                result[i + j + 1] += result[i + j] // 10
-                result[i + j] %= 10
+                pos[p1] += total_sum // 10
+                pos[p2] = total_sum % 10
 
-        while len(result) > 1 and result[-1] == 0:
-            result.pop()
-
-        result_str: str = "".join(map(str, result[::-1]))
-
-        return result_str
+        result = "".join(map(str, pos)).lstrip("0")
+        return result if result else "0"
