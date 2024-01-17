2afac794455a5570e4d0eb99e43f9b9c10d90afc
Dat Tran <datttrian@gmail.com>
2024-01-17 23:36:41 +0000

    Wed, Jan 17, 2024, 11:36 PM +00:00

--- leetcode/set-matrix-zeroes/set_matrix_zeroes.py
+++ leetcode/set-matrix-zeroes/set_matrix_zeroes.py
@@ -1,31 +1,19 @@
 class Solution:
     def setZeroes(self, matrix: list[list[int]]) -> None:
         """Do not return anything, modify matrix in-place instead."""
-        if not matrix or not matrix[0]:
-            return
-
         rows, cols = len(matrix), len(matrix[0])
-        first_row_has_zero = any(matrix[0][j] == 0 for j in range(cols))
-        first_col_has_zero = any(matrix[i][0] == 0 for i in range(rows))
+        zero_rows, zero_cols = set(), set()
 
-        # Use the first row and first column to mark if the corresponding row
-        # or column should be set to zero
-        for i in range(1, rows):
-            for j in range(1, cols):
-                if matrix[i][j] == 0:
-                    matrix[i][0] = matrix[0][j] = 0
-
-        # Set zeros based on markings in the first row and first column
-        for i in range(1, rows):
-            for j in range(1, cols):
-                if matrix[i][0] == 0 or matrix[0][j] == 0:
-                    matrix[i][j] = 0
-
-        # Set zeros for the first row and first column if necessary
-        if first_row_has_zero:
+        for i in range(rows):
             for j in range(cols):
-                matrix[0][j] = 0
+                if matrix[i][j] == 0:
+                    zero_rows.add(i)
+                    zero_cols.add(j)
 
-        if first_col_has_zero:
+        for row in zero_rows:
+            for j in range(cols):
+                matrix[row][j] = 0
+
+        for col in zero_cols:
             for i in range(rows):
-                matrix[i][0] = 0
+                matrix[i][col] = 0
