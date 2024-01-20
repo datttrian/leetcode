f5828096b41400bb402adf6e19bcbdc3391fd126
Dat Tran <datttrian@gmail.com>
2024-01-20 15:47:08 +0000

    Sat, Jan 20, 2024, 3:47 PM +00:00

--- style.sh
+++
@@ -1,5 +0,0 @@
-set -xe
-python -m isort leetcode --check-only
-flake8 leetcode --show-source
-pylint leetcode
-mypy leetcode
--- tests.sh
+++
@@ -1,2 +0,0 @@
-# pytest --cache-clear --junitxml=pytest.xml --cov=leetcode leetcode/ | tee pytest-coverage.txt
-pytest --cache-clear --junitxml=pytest.xml leetcode/ | tee pytest-coverage.txt
