3ade1ff2a91ba00d379711e063725994a8592920
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-05 23:36:08 +0000

    Mon, Feb 5, 2024, 11:36 PM +00:00

--- leetcode/two-sum/test_two_sum.py
+++ leetcode/two-sum/test_two_sum.py
@@ -3,7 +3,7 @@ from two_sum import Solution
 
 
 @pytest.mark.parametrize(
-    ('nums', 'target', 'expected'),
+    ("nums", "target", "expected"),
     [
         ([2, 7, 11, 15], 9, [0, 1]),
         ([3, 2, 4], 6, [1, 2]),
