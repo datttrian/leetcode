d54320fa72b13c08b619c992dfccb01dd0423b83
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-29 22:14:37 +0000

    Thu, Feb 29, 2024, 10:14 PM +00:00

--- leetcode/two-sum/two_sum.py
+++ leetcode/two-sum/two_sum.py
@@ -20,8 +20,8 @@ class Solution:
 
 
 if __name__ == "__main__":
-    nums = [2, 7, 11, 15]
-    target = 9
+    nums = [3, 2, 4]
+    target = 6
 
     # Place a breakpoint at the line of function call
     # For example:
