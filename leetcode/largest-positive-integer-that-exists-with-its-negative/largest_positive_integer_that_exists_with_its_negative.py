fbac7565b71edcab60e514bd1c883905ad1ead8f
datttrian <datttrian@gmail.com>
2024-05-02 23:42:28 +0000

    Thu, May 2, 2024, 11:42 PM +00:00

--- leetcode/largest-positive-integer-that-exists-with-its-negative/largest_positive_integer_that_exists_with_its_negative.py
+++ leetcode/largest-positive-integer-that-exists-with-its-negative/largest_positive_integer_that_exists_with_its_negative.py
@@ -1,20 +1,11 @@
 class Solution:
     def findMaxK(self, nums: list[int]) -> int:
-
-        nums.sort()
-        left, right = 0, len(nums) - 1
+        nums_set = set(nums)
         largest_k = -1
 
-        while left < right:
-            current_sum = nums[left] + nums[right]
-            if current_sum == 0:
-                if nums[right] > 0:
-                    largest_k = max(largest_k, nums[right])
-                left += 1
-                right -= 1
-            elif current_sum > 0:
-                right -= 1
-            else:
-                left += 1
+        for num in nums:
+            if num > 0 and -num in nums_set:
+                if num > largest_k:
+                    largest_k = num
 
         return largest_k
