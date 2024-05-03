f2def4ca8c42d6c2c5ca502b6a1797844ee8ccfa
datttrian <datttrian@gmail.com>
2024-05-03 23:33:47 +0000

    Fri, May 3, 2024, 11:33 PM +00:00

---
+++ leetcode/compare-version-numbers/compare_version_numbers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        parts1 = version1.split(".")
+        parts2 = version2.split(".")
+
+        max_length = max(len(parts1), len(parts2))
+
+        for i in range(max_length):
+            num1 = int(parts1[i]) if i < len(parts1) else 0
+            num2 = int(parts2[i]) if i < len(parts2) else 0
+
+            if num1 > num2:
+                return 1
+            elif num1 < num2:
+                return -1
+
+        return 0
