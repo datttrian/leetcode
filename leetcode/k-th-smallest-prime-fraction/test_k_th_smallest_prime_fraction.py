5a8b5680d8a2c2b4f66aa132cdff81f7d60cab3d
datttrian <datttrian@gmail.com>
2024-05-10 23:59:48 +0000

    Fri, May 10, 2024, 11:59 PM +00:00

--- leetcode/k-th-smallest-prime-fraction/test_k_th_smallest_prime_fraction.py
+++ leetcode/k-th-smallest-prime-fraction/test_k_th_smallest_prime_fraction.py
@@ -8,11 +8,9 @@ from k_th_smallest_prime_fraction import (
     "arr, k, expected",
     [
         ([1, 2, 3, 5], 3, [2, 5]),
-        (
-            [3, 5],
-            1,
-            [3, 5],
-        ),
+        ([3, 5], 1, [3, 5]),
+        ([2, 3], 1, [2, 3]),
+        ([11, 29, 47, 59], 4, [29, 59]),
     ],
 )
 def test_kth_smallest_prime_fraction(
