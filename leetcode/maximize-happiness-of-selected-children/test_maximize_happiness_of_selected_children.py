9d47835837a4aaca62955352f83e17b55332e0fb
datttrian <datttrian@gmail.com>
2024-05-10 00:00:11 +0000

    Thu, May 9, 2024, 11:59 PM +00:00

---
+++ leetcode/maximize-happiness-of-selected-children/test_maximize_happiness_of_selected_children.py
@@ -0,0 +1,15 @@
+import pytest
+from maximize_happiness_of_selected_children import Solution
+
+
+@pytest.mark.parametrize(
+    "happiness,k,expected",
+    [
+        ([1, 2, 3], 1, 3),
+    ],
+)
+def test_maximumHappinessSum(
+    happiness: list[int], k: int, expected: int
+) -> None:
+    solution = Solution()
+    assert solution.maximumHappinessSum(happiness, k) == expected
