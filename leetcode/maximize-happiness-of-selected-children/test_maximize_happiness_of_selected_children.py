d08e3dfae4e3764caca8bc4455862330814ef0ad
Dat Thanh Tran <datttrian@gmail.com>
2024-05-13 22:57:22 +0000

    Mon, May 13, 2024, 10:57 PM +00:00

--- leetcode/maximize-happiness-of-selected-children/test_maximize_happiness_of_selected_children.py
+++ leetcode/maximize-happiness-of-selected-children/test_maximize_happiness_of_selected_children.py
@@ -8,8 +8,6 @@ from maximize_happiness_of_selected_children import Solution
         ([1, 2, 3], 1, 3),
     ],
 )
-def test_maximumHappinessSum(
-    happiness: list[int], k: int, expected: int
-) -> None:
+def test_maximumHappinessSum(happiness: list[int], k: int, expected: int) -> None:
     solution = Solution()
     assert solution.maximumHappinessSum(happiness, k) == expected
