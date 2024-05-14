023dfa474825c07e7dfc4b53461b43602b29d9b3
Dat Thanh Tran <datttrian@gmail.com>
2024-05-14 23:31:20 +0000

    Tue, May 14, 2024, 11:31 PM +00:00

--- leetcode/path-with-maximum-gold/path_with_maximum_gold.py
+++ leetcode/path-with-maximum-gold/path_with_maximum_gold.py
@@ -1,3 +1,30 @@
 class Solution:
     def getMaximumGold(self, grid: list[list[int]]) -> int:
-        return 0
+        def dfs(x: int, y: int) -> int:
+            if (
+                x < 0
+                or x >= len(grid)
+                or y < 0
+                or y >= len(grid[0])
+                or grid[x][y] == 0
+            ):
+                return 0
+
+            current_gold = grid[x][y]
+            grid[x][y] = 0
+
+            max_gold = 0
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                max_gold = max(max_gold, dfs(x + dx, y + dy))
+
+            grid[x][y] = current_gold
+
+            return current_gold + max_gold
+
+        max_gold_collected = 0
+        for i, row in enumerate(grid):
+            for j, cell in enumerate(row):
+                if cell != 0:
+                    max_gold_collected = max(max_gold_collected, dfs(i, j))
+
+        return max_gold_collected
