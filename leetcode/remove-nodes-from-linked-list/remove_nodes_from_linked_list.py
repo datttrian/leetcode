07686223dff0625a8b603d5b6c50e99d2f0fc0d7
Dat Thanh Tran <datttrian@gmail.com>
2024-05-16 23:53:20 +0000

    Thu, May 16, 2024, 11:53 PM +00:00

---
+++ leetcode/remove-nodes-from-linked-list/remove_nodes_from_linked_list.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(
+        self,
+        val: int = 0,
+        next_node: "Optional[ListNode]" = None,
+    ) -> None:
+        self.val: int = val
+        self.next: Optional[ListNode] = next_node
+
+
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        new_head = self.removeNodes(head.next)
+        if new_head and head.val < new_head.val:
+            return new_head
+        head.next = new_head
+        return head
