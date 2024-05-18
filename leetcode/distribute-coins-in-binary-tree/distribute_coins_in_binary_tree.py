44efd174741805674aebc0bc18f9603c82484fd6
Dat Thanh Tran <datttrian@gmail.com>
2024-05-18 01:05:47 +0000

    Sat, May 18, 2024, 1:05 AM +00:00

---
+++ leetcode/distribute-coins-in-binary-tree/distribute_coins_in_binary_tree.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(
+        self,
+        val: int = 0,
+        left: Optional["TreeNode"] = None,
+        right: Optional["TreeNode"] = None,
+    ) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def distributeCoins(self, root: Optional[TreeNode]) -> int:
+        return 0
