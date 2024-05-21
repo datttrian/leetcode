dee523da91ee34fb428cefd00282f20f3a4ff4ee
Dat Thanh Tran <datttrian@gmail.com>
2024-05-21 23:31:52 +0000

    Tue, May 21, 2024, 11:31 PM +00:00

--- leetcode/subsets/subsets.py
+++ leetcode/subsets/subsets.py
@@ -1,25 +1,6 @@
-def subsets(nums: list[int]) -> list[list[int]]:
-    res: list[list[int]] = [[]]
-    for num in nums:
-        res += [curr + [num] for curr in res]
-    return res
-
-
-# Example usage:
-nums1: list[int] = []
-print(subsets(nums1))
-
-nums2 = [-5]
-print(subsets(nums2))
-
-nums3 = [-1, 1]
-print(subsets(nums3))
-
-nums4 = [7, 7]
-print(subsets(nums4))
-
-nums5 = [1, 2, 3]
-print(subsets(nums5))
-
-nums6 = [1, 2, 3, 4, 5]
-print(subsets(nums6))
+class Solution:
+    def subsets(self, nums: list[int]) -> list[list[int]]:
+        res: list[list[int]] = [[]]
+        for num in nums:
+            res += [curr + [num] for curr in res]
+        return res
