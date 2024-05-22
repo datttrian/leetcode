6c36dfbed54a6cbeb39241b852b4d23009413862
Dat Thanh Tran <datttrian@gmail.com>
2024-05-22 17:34:48 +0000

    Wed, May 22, 2024, 5:34 PM +00:00

--- leetcode/palindrome-partitioning/palindrome_partitioning.py
+++ leetcode/palindrome-partitioning/palindrome_partitioning.py
@@ -2,6 +2,7 @@ from typing import Tuple
 
 
 def get_partitions(s: str) -> list[list[str]]:
+
     partitions: list[Tuple[list[str], int]] = [([s], 0)]
     result: list[list[str]] = []
 
@@ -11,15 +12,16 @@ def get_partitions(s: str) -> list[list[str]]:
         if index == len(s):
             result.append(partition)
         else:
+
             for i in range(index + 1, len(s) + 1):
                 new_partition = (
                     partition[:-1]
                     + [partition[-1][: i - index]]
                     + [partition[-1][i - index :]]
                 )
-                # Skip adding partition if it results in an empty string
-                if new_partition[-1]:
-                    partitions.append((new_partition, i))
+
+                new_partition = [part for part in new_partition if part]
+                partitions.append((new_partition, i))
 
     return result
 
@@ -30,10 +32,9 @@ def is_palindrome(sub: str) -> bool:
 
 s = "aab"
 partitions = get_partitions(s)
-print(partitions)
-# result = [
-#     partition
-#     for partition in partitions
-#     if all(is_palindrome(sub) for sub in partition)
-# ]
-# print(result)
+result = [
+    partition
+    for partition in partitions
+    if all(is_palindrome(sub) for sub in partition)
+]
+print(result)
