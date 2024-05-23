770129b3a3a2fb8e7445f8ba9f27b891dfd69808
Dat Thanh Tran <datttrian@gmail.com>
2024-05-23 23:13:12 +0000

    Thu, May 23, 2024, 11:13 PM +00:00

--- checks.sh
+++ checks.sh
@@ -1,4 +1,4 @@
 set -xe
-isort leetcode --check-only
+python -m isort leetcode
 docker run -e VALIDATE_EDITORCONFIG=false -e VALIDATE_GITLEAKS=false -e VALIDATE_JSCPD=false -e VALIDATE_JSON=false -e VALIDATE_MARKDOWN=false -e RUN_LOCAL=true -v "$(pwd)":/tmp/lint github/super-linter
 pytest leetcode
