43c251e3d70a2b6b8ed2763049738f6ffb2618be
Dat Thanh Tran <datttrian@gmail.com>
2024-05-23 23:38:45 +0000

    Thu, May 23, 2024, 11:38 PM +00:00

--- leetcode/the-number-of-beautiful-subsets/the_number_of_beautiful_subsets.py
+++ leetcode/the-number-of-beautiful-subsets/the_number_of_beautiful_subsets.py
@@ -1,22 +1,20 @@
-def getSubsets(nums: list[int]) -> list[list[int]]:
-    res: list[list[int]] = []
-    for num in nums:
-        res += [curr + [num] for curr in res]
-        res.append([num])
-    return res
+class Solution:
+    def beautifulSubsets(self, nums: list[int], k: int) -> int:
+        def getSubsets(nums: list[int]) -> list[list[int]]:
+            res: list[list[int]] = []
+            for num in nums:
+                res += [curr + [num] for curr in res]
+                res.append([num])
+            return res
 
+        def isBeautiful(nums: list[int], k: int) -> bool:
+            nums_set = set(nums)
+            for num in nums:
+                if (num + k) in nums_set or (num - k) in nums_set:
+                    return False
+            return True
 
-def isBeautiful(nums: list[int], k: int) -> bool:
-    nums_set = set(nums)
-    for num in nums:
-        if (num + k) in nums_set or (num - k) in nums_set:
-            return False
-    return True
+        subsets = getSubsets(nums)
+        result = [subset for subset in subsets if isBeautiful(subset, k)]
 
-
-nums = [2, 4, 6]
-subsets = getSubsets(nums)
-print(subsets)
-
-result = [subset for subset in subsets if isBeautiful(subset, 2)]
-print(len(result))
+        return len(result)
