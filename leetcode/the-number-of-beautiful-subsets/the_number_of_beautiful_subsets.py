874fe1d6411f929d4ea56211064f658c0dd414be
Dat Thanh Tran <datttrian@gmail.com>
2024-05-23 23:25:01 +0000

    Thu, May 23, 2024, 11:25 PM +00:00

--- leetcode/the-number-of-beautiful-subsets/the_number_of_beautiful_subsets.py
+++ leetcode/the-number-of-beautiful-subsets/the_number_of_beautiful_subsets.py
@@ -1,10 +1,12 @@
 def get_subsets(nums: list[int]) -> list[list[int]]:
-    res: list[list[int]] = [[]]
+    res: list[list[int]] = []
     for num in nums:
         res += [curr + [num] for curr in res]
+        res.append([num])
     return res
 
 
 # Example usage:
 nums = [1, 2, 3]
-print(get_subsets(nums))
+subsets = get_subsets(nums)
+print(subsets)
