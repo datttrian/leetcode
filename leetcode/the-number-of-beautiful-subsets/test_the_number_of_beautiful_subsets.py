0483c6083c70fe352c39e0952606cc188b8e77e5
Dat Thanh Tran <datttrian@gmail.com>
2024-05-23 23:43:32 +0000

    Thu, May 23, 2024, 11:43 PM +00:00

--- leetcode/the-number-of-beautiful-subsets/test_the_number_of_beautiful_subsets.py
+++ leetcode/the-number-of-beautiful-subsets/test_the_number_of_beautiful_subsets.py
@@ -5,10 +5,24 @@ from the_number_of_beautiful_subsets import Solution
 @pytest.mark.parametrize(
     ["nums", "k", "expected"],
     [
-        ([1, 2, 3], 1, 7),
-        ([1, 2, 3], 2, 7),
-        ([1, 5, 9], 4, 7),
-        ([1, 1, 2], 1, 4),
+        ([1, 2, 3], 1, 4),
+        ([], 1, 0),
+        ([5], 3, 1),
+        ([2, 2, 2], 1, 1),
+        ([1, 2, 3, 4], 10, 15),
+        (
+            [1, 3, 5, 7],
+            2,
+            15,
+        ),
+        ([1, 1, 1], 0, 1),
+        (
+            [-3, -2, -1, 1, 2, 3],
+            1,
+            14,
+        ),
+        ([1, 2, 3, 4, 5, 6], 2, 32),
+        ([4, 4, 5, 6, 7], 2, 16),
     ],
 )
 def test_beautifulSubsets(nums: list[int], k: int, expected: int) -> None:
