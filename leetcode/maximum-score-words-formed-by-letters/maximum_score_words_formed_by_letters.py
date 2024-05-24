c03ded241f5aa980e4593c7d11c8321dd6efa338
Dat Thanh Tran <datttrian@gmail.com>
2024-05-24 23:06:39 +0000

    Fri, May 24, 2024, 11:06 PM +00:00

--- leetcode/maximum-score-words-formed-by-letters/maximum_score_words_formed_by_letters.py
+++ leetcode/maximum-score-words-formed-by-letters/maximum_score_words_formed_by_letters.py
@@ -33,3 +33,17 @@ score_dict = {chr(97 + i): score[i] for i in range(len(score))}
 
 # Print the dictionary
 print(score_dict)
+
+# Given list of letters
+letters = ["a", "a", "c", "d", "d", "d", "g", "o", "o"]
+
+# Create dictionary to count occurrences of each letter
+letter_counts: dict[str, int] = {}
+for letter in letters:
+    if letter in letter_counts:
+        letter_counts[letter] += 1
+    else:
+        letter_counts[letter] = 1
+
+# Print the dictionary
+print(letter_counts)
