20228435c06d36a0458f9b4b66b5ab477d68b001
datttrian <datttrian@gmail.com>
2024-06-03 23:06:12 +0000

    Mon, Jun 3, 2024, 11:06 PM +00:00

---
+++ leetcode/valid-anagram/valid_anagram_4.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count: dict[str, int] = {}
+
+        for char in s:
+            if char in count:
+                count[char] += 1
+            else:
+                count[char] = 1
+
+        for char in t:
+            if char in count:
+                count[char] -= 1
+            else:
+                return False
+
+        for char in count:
+            if count[char] != 0:
+                return False
+
+        return True
+
+
+solution = Solution()
+print(solution.isAnagram(s="anagram", t="nagaram"))
+print(solution.isAnagram(s="rat", t="car"))
