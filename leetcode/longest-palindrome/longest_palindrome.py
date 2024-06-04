ebc1982fe36c4fe3d2423f2826d2ac05863ff95f
datttrian <datttrian@gmail.com>
2024-06-04 23:04:59 +0000

    Tue, Jun 4, 2024, 11:04 PM +00:00

---
+++ leetcode/longest-palindrome/longest_palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        return 0
+
+
+solution = Solution()
+print(solution.longestPalindrome(s="abccccdd"))
+print(solution.longestPalindrome(s="a"))
