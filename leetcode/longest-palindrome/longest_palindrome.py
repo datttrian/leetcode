40c25dd120f10075eadd8e2c2fea868d351a0f44
datttrian <datttrian@gmail.com>
2024-06-04 23:30:28 +0000

    Tue, Jun 4, 2024, 11:30 PM +00:00

--- leetcode/longest-palindrome/longest_palindrome.py
+++ leetcode/longest-palindrome/longest_palindrome.py
@@ -1,11 +1,8 @@
 from collections import Counter
 
-s = "abccccdd"
 
-count = Counter(s)
-print(count)
-print(list((c // 2) * 2 for c in count.values()))
-
-# length = sum()
-
-print(list((c % 2 == 1 for c in count.values())))
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        char_count = Counter(s)
+        length = sum(count // 2 * 2 for count in char_count.values())
+        return length + any(count % 2 for count in char_count.values())
