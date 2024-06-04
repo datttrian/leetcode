2e60c00e3c5bc3404a24e00c66b4bc141f3109b5
datttrian <datttrian@gmail.com>
2024-06-04 23:24:47 +0000

    Tue, Jun 4, 2024, 11:24 PM +00:00

--- leetcode/longest-palindrome/longest_palindrome.py
+++ leetcode/longest-palindrome/longest_palindrome.py
@@ -3,7 +3,9 @@ from collections import Counter
 s = "abccccdd"
 
 count = Counter(s)
+print(count)
+print(list((c // 2) * 2 for c in count.values()))
 
-length = sum((c // 2) * 2 for c in count.values())
+# length = sum()
 
 print(list((c % 2 == 1 for c in count.values())))
