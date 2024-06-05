6a9e5bafcc1c11072a104fd6326b1b99c6826049
datttrian <datttrian@gmail.com>
2024-06-05 18:09:39 +0000

    Wed, Jun 5, 2024, 6:09 PM +00:00

--- leetcode/valid-palindrome/valid_palindrome_1.py
+++ leetcode/valid-palindrome/valid_palindrome_1.py
@@ -2,3 +2,9 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         normalized_s = "".join(char.lower() for char in s if char.isalnum())
         return normalized_s == normalized_s[::-1]
+
+
+solution = Solution()
+print(solution.isPalindrome(s="A man, a plan, a canal: Panama"))
+print(solution.isPalindrome(s="race a car"))
+print(solution.isPalindrome(s=" "))
