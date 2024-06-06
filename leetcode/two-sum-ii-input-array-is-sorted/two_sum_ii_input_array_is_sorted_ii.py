c5c80501d757ba31a45a2cbc65a6679666931971
datttrian <datttrian@gmail.com>
2024-06-06 23:26:29 +0000

    Thu, Jun 6, 2024, 11:26 PM +00:00

--- leetcode/two-sum-ii-input-array-is-sorted/two_sum_ii_input_array_is_sorted_ii.py
+++ leetcode/two-sum-ii-input-array-is-sorted/two_sum_ii_input_array_is_sorted_ii.py
@@ -1,16 +1,20 @@
-numbers = [2,3,4]
-target = 6
-target=9
-numbers_index = list(enumerate(numbers))
-left, right = 0, len(numbers) - 1
+class Solution:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        nums_index = list(enumerate(numbers))
+        left, right = 0, len(numbers) - 1
 
-while left < right:
-    sum = numbers_index[left][1] + numbers_index[right][1]
-    if sum == target:
-        print([numbers_index[left][0]+1, numbers_index[right][0]+1]) 
-        break
-    elif sum < target:
-        left += 1
-    else:
-        right -= 1
+        while left < right:
+            sum = nums_index[left][1] + nums_index[right][1]
+            if sum == target:
+                return [nums_index[left][0] + 1, nums_index[right][0] + 1]
+            elif sum < target:
+                left += 1
+            else:
+                right -= 1
+        return []
 
+
+solution = Solution()
+print(solution.twoSum(numbers=[2, 7, 11, 15], target=9))
+print(solution.twoSum(numbers=[2, 3, 4], target=6))
+print(solution.twoSum(numbers=[-1, 0], target=-1))
