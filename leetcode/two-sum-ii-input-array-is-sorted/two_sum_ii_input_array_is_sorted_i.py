92af01543bef9ec48a851fac5dadd9b1ddfe5289
datttrian <datttrian@gmail.com>
2024-06-06 23:31:08 +0000

    Thu, Jun 6, 2024, 11:31 PM +00:00

--- leetcode/two-sum-ii-input-array-is-sorted/two_sum_ii_input_array_is_sorted_i.py
+++ leetcode/two-sum-ii-input-array-is-sorted/two_sum_ii_input_array_is_sorted_i.py
@@ -12,5 +12,5 @@ class Solution:
 
 solution = Solution()
 print(solution.twoSum(numbers=[2, 7, 11, 15], target=9))
-print(solution.twoSum(numbers=[3, 2, 4], target=6))
-print(solution.twoSum(numbers=[3, 3], target=6))
+print(solution.twoSum(numbers=[2, 3, 4], target=6))
+print(solution.twoSum(numbers=[-1, 0], target=-1))
