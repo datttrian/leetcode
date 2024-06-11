58ea8894ab035625e769bf115ad6d620c81c70c5
datttrian <datttrian@gmail.com>
2024-06-11 23:40:28 +0000

    Tue, Jun 11, 2024, 11:40 PM +00:00

--- leetcode/relative-sort-array/relative_sort_array.py
+++ leetcode/relative-sort-array/relative_sort_array.py
@@ -2,4 +2,5 @@ class Solution:
     def relativeSortArray(self, arr1: list[int], arr2: list[int]) -> list[int]:
         result = [num for num in arr2 for _ in range(arr1.count(num))]
         arr1 = [num for num in arr1 if num not in arr2]
-        return result + sorted(arr1)
+        arr1.sort()
+        return result + arr1
