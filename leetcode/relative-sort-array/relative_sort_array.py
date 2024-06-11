e6f403545e7b54c60223e38b0e86342330a48531
datttrian <datttrian@gmail.com>
2024-06-11 23:22:41 +0000

    Tue, Jun 11, 2024, 11:22 PM +00:00

--- leetcode/relative-sort-array/relative_sort_array.py
+++ leetcode/relative-sort-array/relative_sort_array.py
@@ -1,11 +1,6 @@
-from typing import Counter
-
-
 arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19]
 arr2 = [2, 1, 4, 3, 9, 6]
 
-count = Counter(arr1)
-print(count[1])
 result = []
 
 for i in arr2:
@@ -16,4 +11,6 @@ for i in arr2:
 
 print(arr1)
 print(result)  # type: ignore
-result.append(arr1)  # type: ignore
+for num in arr1:
+    result.append(num)  # type: ignore
+print(result)  # type: ignore
