4afca44c44c7ae0c02ba791cc1c5e2bd15cbdc98
datttrian <datttrian@gmail.com>
2024-06-11 23:03:55 +0000

    Tue, Jun 11, 2024, 11:03 PM +00:00

--- leetcode/relative-sort-array/relative_sort_array.py
+++ leetcode/relative-sort-array/relative_sort_array.py
@@ -2,8 +2,4 @@ arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19]
 arr2 = [2, 1, 4, 3, 9, 6]
 
 
-def relativeSortArray(arr1: list[int], arr2: list[int]) -> list[int]:
-    return []
-
-
-print(relativeSortArray(arr1, arr2))
+print(sorted(arr1))
