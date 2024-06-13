8c1e8293d9aade3226fca82ea69a01ef17801259
Dat Thanh Tran <datttrian@gmail.com>
2024-06-13 23:13:32 +0000

    Thu, Jun 13, 2024, 11:13 PM +00:00

--- src/valid-anagram/valid_anagram_1.py
+++ src/valid-anagram/valid_anagram_1.py
@@ -3,14 +3,12 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        t_list = list(t)
-
         for char in s:
             is_matched = False
 
-            for i, t_char in enumerate(t_list):
-                if t_char == char:
-                    t_list.pop(i)
+            for i in range(len(t)):
+                if t[i] == char:
+                    t = t[:i] + t[i + 1 :]
                     is_matched = True
                     break
 
