c1b0f5b90e90faddc52155d34f1ca3915f3f672e
datttrian <datttrian@gmail.com>
2024-06-14 20:06:02 +0000

    Fri, Jun 14, 2024, 8:06 PM +00:00

---
+++ src/best-time-to-buy-and-sell-stock/best_time_to_buy_and_sell_stock_1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        n = len(prices)
+        max_profit = 0
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                profit = prices[j] - prices[i]
+                max_profit = max(profit, max_profit)
+
+        return max_profit
+
+
+solution = Solution()
+print(solution.maxProfit(prices=[7, 1, 5, 3, 6, 4]))
+print(solution.maxProfit(prices=[7, 6, 4, 3, 1]))
