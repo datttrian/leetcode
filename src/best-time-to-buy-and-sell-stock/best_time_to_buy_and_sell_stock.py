b55a6e465028b06d89bec3814520f276f55a3dfe
datttrian <datttrian@gmail.com>
2024-06-14 19:54:24 +0000

    Fri, Jun 14, 2024, 7:54 PM +00:00

--- src/best-time-to-buy-and-sell-stock/best_time_to_buy_and_sell_stock.py
+++ src/best-time-to-buy-and-sell-stock/best_time_to_buy_and_sell_stock.py
@@ -13,3 +13,8 @@ class Solution:
                 max_profit = max(max_profit, price - min_price)
 
         return max_profit
+
+
+solution = Solution()
+print(solution.maxProfit(prices=[7, 1, 5, 3, 6, 4]))
+print(solution.maxProfit(prices=[7, 6, 4, 3, 1]))
