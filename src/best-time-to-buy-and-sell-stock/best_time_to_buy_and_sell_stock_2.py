b6aa2d101539177b15881e8cf4497973fd7f7a6c
datttrian <datttrian@gmail.com>
2024-06-14 20:27:39 +0000

    Fri, Jun 14, 2024, 8:27 PM +00:00

--- src/best-time-to-buy-and-sell-stock/best_time_to_buy_and_sell_stock_2.py
+++ src/best-time-to-buy-and-sell-stock/best_time_to_buy_and_sell_stock_2.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        min_price = float("inf")
+        min_price = prices[0]
         max_profit = 0
 
         for price in prices:
