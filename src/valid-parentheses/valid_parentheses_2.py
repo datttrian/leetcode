ea6759a37bedb5488e612c894d0a5bf76497c8ab
datttrian <datttrian@gmail.com>
2024-06-17 09:13:25 +0000

    Mon, Jun 17, 2024, 9:13 AM +00:00

--- src/valid-parentheses/valid_parentheses_2.py
+++ src/valid-parentheses/valid_parentheses_2.py
@@ -5,13 +5,10 @@ class Solution:
 
         n = len(s)
         i = 0
+        matching_parentheses = {"(": ")", "{": "}", "[": "]"}
 
         while i < n - 1:
-            if (
-                (s[i] == "(" and s[i + 1] == ")")
-                or (s[i] == "{" and s[i + 1] == "}")
-                or (s[i] == "[" and s[i + 1] == "]")
-            ):
+            if s[i] in matching_parentheses and s[i + 1] == matching_parentheses[s[i]]:
                 new_s = s[:i] + s[i + 2 :]
                 return self.isValid(new_s)
             i += 1
