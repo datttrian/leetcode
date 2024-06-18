f818176f7eefa54406adfa73fb4c83cd2c7a6df5
datttrian <datttrian@gmail.com>
2024-06-18 08:40:05 +0000

    Tue, Jun 18, 2024, 8:40 AM +00:00

--- src/binary-search/binary_search_2.py
+++ src/binary-search/binary_search_2.py
@@ -7,16 +7,11 @@ class Solution:
 
             if nums[mid] == target:
                 return mid
-            left, right = (
+
+            return (
                 binary_search(mid + 1, right)
                 if nums[mid] < target
                 else binary_search(left, mid - 1)
             )
-            return -1
 
         return binary_search(0, len(nums) - 1)
-
-
-solution = Solution()
-print(solution.search([-1, 0, 3, 5, 9, 12], target=9))
-print(solution.search([-1, 0, 3, 5, 9, 12], target=2))
