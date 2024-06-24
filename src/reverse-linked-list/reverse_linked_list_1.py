ec1831d052363315b64c7d60e8d8b04c1cfee7c9
Dat Thanh Tran <datttrian@gmail.com>
2024-06-24 22:48:43 +0000

    Mon, Jun 24, 2024, 10:48 PM +00:00

--- src/reverse-linked-list/reverse_linked_list_1.py
+++ src/reverse-linked-list/reverse_linked_list_1.py
@@ -25,31 +25,30 @@ class Solution:
         return node
 
 
-# Helper function to create a linked list from a list
-def create_linked_list(head: list[int]) -> Optional[ListNode]:
-    if not head:
-        return None
-    head_node = ListNode(head[0])
-    current_node = head_node
-    for value in head[1:]:
-        current_node.next = ListNode(value)
-        current_node = current_node.next
-    return head_node
+# Helper function to convert list to linked list
+def list_to_linkedlist(head: list[int]) -> Optional[ListNode]:
+    dummy = ListNode()
+    current = dummy
+    for value in head:
+        current.next = ListNode(value)
+        current = current.next
+    return dummy.next
 
 
-# Helper function to convert a linked list to a list
-def linked_list_to_list(headNode: Optional[ListNode]) -> list[int]:
+# Helper function to convert linked list to list
+def linkedlist_to_list(node: Optional[ListNode]) -> list[int]:
     result: list[int] = []
-    current_node = headNode
-    while current_node:
-        result.append(current_node.val)
-        current_node = current_node.next
+    while node:
+        result.append(node.val)
+        node = node.next
     return result
 
 
 solution = Solution()
 print(
-    linked_list_to_list(solution.reverseList(create_linked_list(head=[1, 2, 3, 4, 5])))
+    linkedlist_to_list(solution.reverseList(
+        list_to_linkedlist(head=[1, 2, 3, 4, 5])))
 )
-print(linked_list_to_list(solution.reverseList(create_linked_list(head=[1, 2]))))
-print(linked_list_to_list(solution.reverseList(create_linked_list(head=[]))))
+print(linkedlist_to_list(solution.reverseList(
+    list_to_linkedlist(head=[1, 2]))))
+print(linkedlist_to_list(solution.reverseList(list_to_linkedlist(head=[]))))
