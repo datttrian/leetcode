0571cba8d24be56f35a1ed26e90438063732b1c3
Dat Thanh Tran <datttrian@gmail.com>
2024-06-24 23:19:17 +0000

    Mon, Jun 24, 2024, 11:19 PM +00:00

--- src/reverse-linked-list/reverse_linked_list_1.py
+++ src/reverse-linked-list/reverse_linked_list_1.py
@@ -23,28 +23,3 @@ class Solution:
             head = temp
 
         return node
-
-
-# Helper function to convert list to linked list
-def list_to_linkedlist(lst: list[int]) -> Optional[ListNode]:
-    dummy = ListNode()
-    current = dummy
-    for value in lst:
-        current.next = ListNode(value)
-        current = current.next
-    return dummy.next
-
-
-# Helper function to convert linked list to list
-def linkedlist_to_list(node: Optional[ListNode]) -> list[int]:
-    result: list[int] = []
-    while node:
-        result.append(node.val)
-        node = node.next
-    return result
-
-
-solution = Solution()
-print(linkedlist_to_list(solution.reverseList(list_to_linkedlist([1, 2, 3, 4, 5]))))
-print(linkedlist_to_list(solution.reverseList(list_to_linkedlist([1, 2]))))
-print(linkedlist_to_list(solution.reverseList(list_to_linkedlist([]))))
--- src/reverse-linked-list/reverse_linked_list_2.py
+++ src/reverse-linked-list/reverse_linked_list_2.py
@@ -21,28 +21,3 @@ class Solution:
         head.next.next = head
         head.next = None
         return new_head
-
-
-# Helper function to convert list to linked list
-def list_to_linkedlist(lst: list[int]) -> Optional[ListNode]:
-    dummy = ListNode()
-    current = dummy
-    for value in lst:
-        current.next = ListNode(value)
-        current = current.next
-    return dummy.next
-
-
-# Helper function to convert linked list to list
-def linkedlist_to_list(node: Optional[ListNode]) -> list[int]:
-    result: list[int] = []
-    while node:
-        result.append(node.val)
-        node = node.next
-    return result
-
-
-solution = Solution()
-print(linkedlist_to_list(solution.reverseList(list_to_linkedlist([1, 2, 3, 4, 5]))))
-print(linkedlist_to_list(solution.reverseList(list_to_linkedlist([1, 2]))))
-print(linkedlist_to_list(solution.reverseList(list_to_linkedlist([]))))
