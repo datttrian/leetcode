c9209d02bdc5fab937cc6a57e9bb0f0faa5b784e
Dat Thanh Tran <datttrian@gmail.com>
2024-06-25 22:52:09 +0000

    Tue, Jun 25, 2024, 10:52 PM +00:00

--- src/binary-search-tree-to-greater-sum-tree/main.py
+++ src/binary-search-tree-to-greater-sum-tree/main.py
@@ -13,58 +13,21 @@ class TreeNode:
         self.right = right
 
 
-root = [4, 1, 6, 0, 2, 5, 7, None, None, None, 3, None, None, None, 8]
+class Solution:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        stack: list[TreeNode] = []
+        node = root
+        total = 0
 
+        while stack or node:
+            while node:
+                stack.append(node)
+                node = node.right
 
-def list_to_tree(lst):  # type: ignore
-    if not lst:
-        return None
-    root = TreeNode(lst[0])  # type: ignore
-    queue = [root]
-    i = 1
-    while i < len(lst):  # type: ignore
-        current = queue.pop(0)
-        if lst[i] is not None:
-            current.left = TreeNode(lst[i])  # type: ignore
-            queue.append(current.left)
-        i += 1
-        if i < len(lst) and lst[i] is not None:  # type: ignore
-            current.right = TreeNode(lst[i])  # type: ignore
-            queue.append(current.right)
-        i += 1
-    return root
+            node = stack.pop()
+            total += node.val
+            node.val = total
 
+            node = node.left
 
-root_tree = list_to_tree(root)
-print(root_tree)
-
-
-stack = []
-print(stack)  # type: ignore
-print(root_tree)
-stack.append(root_tree)  # type: ignore
-print(stack)  # type: ignore
-print(root_tree.left)  # type: ignore
-
-
-node = root_tree.left  # type: ignore
-print(node.val)  # type: ignore
-
-
-def bstToGst(root: TreeNode) -> TreeNode:
-    stack = []
-    node = root
-    total_sum = 0
-
-    while stack or node:
-        while node:
-            stack.append(node)  # type: ignore
-            node = node.right  # type: ignore
-
-        node = stack.pop()  # type: ignore
-        total_sum += node.val  # type: ignore
-        node.val = total_sum
-
-        node = node.left  # type: ignore
-
-    return root
+        return root
