99383485e27a2b85388f9889efdd4945b5872bd0
Dat Thanh Tran <datttrian@gmail.com>
2024-06-25 22:03:12 +0000

    Tue, Jun 25, 2024, 10:03 PM +00:00

--- src/binary-search-tree-to-greater-sum-tree/main.py
+++ src/binary-search-tree-to-greater-sum-tree/main.py
@@ -1,1 +1,16 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(
+        self,
+        val: int = 0,
+        left: Optional["TreeNode"] = None,
+        right: Optional["TreeNode"] = None,
+    ) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 root = [4, 1, 6, 0, 2, 5, 7, None, None, None, 3, None, None, None, 8]
