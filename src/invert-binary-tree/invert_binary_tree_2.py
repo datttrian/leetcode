4ac855180733762f28d2dd9fe0dada522abd9b7e
Dat Thanh Tran <datttrian@gmail.com>
2024-06-27 23:31:11 +0000

    Thu, Jun 27, 2024, 11:31 PM +00:00

--- src/invert-binary-tree/invert_binary_tree_2.py
+++ src/invert-binary-tree/invert_binary_tree_2.py
@@ -19,57 +19,15 @@ class Solution:
         if not root:
             return None
 
-        root.left, root.right = root.right, root.left
-        self.invertTree(root.left)
-        self.invertTree(root.right)
+        queue = deque([root])
+
+        while queue:
+            current = queue.popleft()
+            current.left, current.right = current.right, current.left
+
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
 
         return root
-
-
-def list_to_tree(lst: list[int]) -> Optional[TreeNode]:
-    if not lst:
-        return None
-
-    root = TreeNode(lst[0])
-    queue = deque([root])
-    i = 1
-
-    while i < len(lst):
-        current = queue.popleft()
-
-        current.left = TreeNode(lst[i])
-        queue.append(current.left)
-        i += 1
-
-        if i < len(lst):
-            current.right = TreeNode(lst[i])
-            queue.append(current.right)
-        i += 1
-
-    return root
-
-
-def tree_to_list(root: Optional[TreeNode]) -> list[Optional[int]]:
-
-    result: list[Optional[int]] = []
-    queue: deque[Optional[TreeNode]] = deque([root])
-
-    while queue:
-        current = queue.popleft()
-        if current:
-            result.append(current.val)
-            queue.append(current.left)
-            queue.append(current.right)
-        else:
-            result.append(None)
-
-    while result and result[-1] is None:
-        result.pop()
-
-    return result
-
-
-solution = Solution()
-print(tree_to_list(solution.invertTree(list_to_tree([4, 2, 7, 1, 3, 6, 9]))))
-print(tree_to_list(solution.invertTree(list_to_tree([2, 1, 3]))))
-print(tree_to_list(solution.invertTree(list_to_tree([]))))
