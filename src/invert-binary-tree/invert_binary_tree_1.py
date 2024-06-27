da2122c7243e76432fdd55f659ab82a4870c17c8
Dat Thanh Tran <datttrian@gmail.com>
2024-06-27 22:32:30 +0000

    Thu, Jun 27, 2024, 10:32 PM +00:00

--- src/invert-binary-tree/invert_binary_tree_1.py
+++ src/invert-binary-tree/invert_binary_tree_1.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import Optional
 
 
@@ -18,11 +19,58 @@ class Solution:
         if not root:
             return None
 
-        # Swap the left and right subtree
         root.left, root.right = root.right, root.left
-
-        # Recursively invert the left and right subtree
         self.invertTree(root.left)
         self.invertTree(root.right)
 
         return root
+
+
+def list_to_tree(nodes: list[int]) -> Optional[TreeNode]:
+    if not nodes:
+        return None
+
+    root = TreeNode(nodes[0])
+    queue = deque([root])
+    i = 1
+
+    while i < len(nodes):
+        current = queue.popleft()
+
+        current.left = TreeNode(nodes[i])
+        queue.append(current.left)
+        i += 1
+
+        if i < len(nodes):
+            current.right = TreeNode(nodes[i])
+            queue.append(current.right)
+        i += 1
+
+    return root
+
+
+def tree_to_list(root: Optional[TreeNode]) -> list[Optional[int]]:
+
+    result: list[Optional[int]] = []
+    queue: deque[Optional[TreeNode]] = deque([root])
+
+    while queue:
+        current = queue.popleft()
+        if current:
+            result.append(current.val)
+            queue.append(current.left)
+            queue.append(current.right)
+        else:
+            result.append(None)
+
+    # Remove trailing None values that are not needed for the representation
+    while result and result[-1] is None:
+        result.pop()
+
+    return result
+
+
+solution = Solution()
+print(tree_to_list(solution.invertTree(list_to_tree([4, 2, 7, 1, 3, 6, 9]))))
+print(tree_to_list(solution.invertTree(list_to_tree([2, 1, 3]))))
+print(tree_to_list(solution.invertTree(list_to_tree([]))))
