e53c1899cafc47c0103d668a7913ec300e5a55cc
Dat Thanh Tran <datttrian@gmail.com>
2024-06-27 22:25:41 +0000

    Thu, Jun 27, 2024, 10:25 PM +00:00

--- src/invert-binary-tree/invert_binary_tree_2.py
+++ src/invert-binary-tree/invert_binary_tree_2.py
@@ -34,3 +34,52 @@ class Solution:
 
         return root
 
+
+def list_to_tree(nodes: list[int]) -> Optional[TreeNode]:
+    if not nodes:
+        return None
+
+    root = TreeNode(nodes[0])
+    queue = deque([root])
+    i = 1
+
+    while i < len(nodes):
+        current = queue.popleft()
+
+        current.left = TreeNode(nodes[i])
+        queue.append(current.left)
+        i += 1
+
+        if i < len(nodes):
+            current.right = TreeNode(nodes[i])
+            queue.append(current.right)
+        i += 1
+
+    return root
+
+
+def tree_to_list(root: Optional[TreeNode]) -> list[Optional[int]]:
+    if not root:
+        return []
+
+    result: list[Optional[int]] = []
+    queue: deque[Optional[TreeNode]] = deque([root])
+
+    while queue:
+        current = queue.popleft()
+        if current:
+            result.append(current.val)
+            queue.append(current.left)
+            queue.append(current.right)
+        else:
+            result.append(None)
+
+    # Remove trailing None values that are not needed for the representation
+    while result and result[-1] is None:
+        result.pop()
+
+    return result
+
+
+solution = Solution()
+print(tree_to_list(solution.invertTree(list_to_tree([4, 2, 7, 1, 3, 6, 9]))))
