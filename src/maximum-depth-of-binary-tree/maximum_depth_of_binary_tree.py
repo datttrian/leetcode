cf94b69b232a3657a9be5d93af4060815dae44f9
Dat Thanh Tran <datttrian@gmail.com>
2024-06-28 22:25:34 +0000

    Fri, Jun 28, 2024, 10:25 PM +00:00

--- src/maximum-depth-of-binary-tree/maximum_depth_of_binary_tree.py
+++ src/maximum-depth-of-binary-tree/maximum_depth_of_binary_tree.py
@@ -16,10 +16,10 @@ class TreeNode:
 
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        return (
+            1 + max(self.maxDepth(root.left),
+                    self.maxDepth(root.right)) if root else 0
+        )
 
 
 def list_to_tree(lst: list[Optional[int]]) -> Optional[TreeNode]:
