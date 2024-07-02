33864dc76db5755fad88b1880a8ab1014476c64b
datttrian <datttrian@gmail.com>
2024-07-02 22:14:48 +0000

    Tue, Jul 2, 2024, 10:14 PM +00:00

--- src/intersection-of-two-arrays-ii/solution.py
+++ src/intersection-of-two-arrays-ii/solution.py
@@ -4,11 +4,4 @@ from typing import Counter
 class Solution:
     def intersect(self, nums1: list[int], nums2: list[int]) -> list[int]:
         count_nums1 = Counter(nums1)
-        intersection: list[int] = []
-
-        for num in nums2:
-            if count_nums1[num]:
-                intersection.append(num)
-                count_nums1[num] -= 1
-
-        return intersection
+        return [num for num in nums2 if count_nums1[num] > 0 and not count_nums1.subtract([num])]
