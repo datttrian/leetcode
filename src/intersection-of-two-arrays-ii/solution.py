d437293af4d0caf527aa44e17717953d7507806f
datttrian <datttrian@gmail.com>
2024-07-02 22:06:07 +0000

    Tue, Jul 2, 2024, 10:06 PM +00:00

--- src/intersection-of-two-arrays-ii/solution.py
+++ src/intersection-of-two-arrays-ii/solution.py
@@ -1,2 +1,17 @@
+from typing import Counter
+
+
 nums1 = [1, 2, 2, 1]
 nums2 = [2, 2]
+intersection = []
+
+count_nums1 = Counter(nums1)
+print(count_nums1)
+
+for num in count_nums1:
+    if num in nums2:
+        intersection.append(num)  # type:ignore
+        count_nums1[num] -= 1  # type:ignore
+
+print(count_nums1)
+print(intersection)  # type:ignore
