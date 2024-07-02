5a92e8f1e9c25a7f75fea3188611fb0cfef83df2
datttrian <datttrian@gmail.com>
2024-07-02 22:12:25 +0000

    Tue, Jul 2, 2024, 10:12 PM +00:00

--- src/intersection-of-two-arrays-ii/solution.py
+++ src/intersection-of-two-arrays-ii/solution.py
@@ -1,30 +1,14 @@
 from typing import Counter
 
 
-nums1 = [1, 2, 2, 1]
-nums2 = [2, 2]
-intersection = []
+class Solution:
+    def intersect(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        count_nums1 = Counter(nums1)
+        intersection: list[int] = []
 
-count_nums1 = Counter(nums1)
+        for num in nums2:
+            if count_nums1[num]:
+                intersection.append(num)
+                count_nums1[num] -= 1
 
-for num in nums2:
-    if count_nums1[num]:
-        intersection.append(num)  # type:ignore
-        count_nums1[num] -= 1  # type:ignore
-
-print(count_nums1)
-print(intersection)  # type:ignore
-
-nums1 = [4, 9, 5]
-nums2 = [9, 4, 9, 8, 4]
-intersection = []
-
-count_nums1 = Counter(nums1)
-
-for num in nums2:
-    if count_nums1[num]:
-        intersection.append(num)  # type:ignore
-        count_nums1[num] -= 1  # type:ignore
-
-print(count_nums1)
-print(intersection)  # type:ignore
+        return intersection
