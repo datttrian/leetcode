4c383c68fd5978091c09dedcdfccd1792205ce10
datttrian <datttrian@gmail.com>
2024-07-04 00:55:49 +0000

    Thu, Jul 4, 2024, 12:55 AM +00:00

--- src/same-tree/same_tree_3.py
+++ src/same-tree/same_tree_3.py
@@ -16,9 +16,7 @@ class TreeNode:
 
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
-            return True
-        if not p or not q or p.val != q.val:
-            return False
+        if not p or not q:
+            return p == q
 
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
