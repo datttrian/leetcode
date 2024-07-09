9911c322172df2d50da0a744e431f0a0211d6eb1
datttrian <datttrian@gmail.com>
2024-07-09 22:13:18 +0000

    Tue, Jul 9, 2024, 10:13 PM +00:00

--- src/average-waiting-time/main.py
+++ src/average-waiting-time/main.py
@@ -6,7 +6,11 @@ arrival_time_2 = customers[2]
 
 print(arrival_time_0)
 
-time_customer_0 = arrival_time_0[0] + arrival_time_0[1]
-print(time_customer_0)
+customer0_finish = arrival_time_0[0] + arrival_time_0[1]
+print(customer0_finish)
 
-print(time_customer_0 + arrival_time_1[1] - arrival_time_1[0])
+customer1_finish = customer0_finish + arrival_time_1[1]
+print(customer1_finish)
+
+customer2_finish = customer1_finish + arrival_time_2[1]
+print(customer2_finish)
