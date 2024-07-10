3ac5b84e03733b17f541c74aeb8cf9e43bbceb39
datttrian <datttrian@gmail.com>
2024-07-10 17:59:46 +0000

    Wed, Jul 10, 2024, 5:59 PM +00:00

--- src/kth-largest-element-in-a-stream/kth_largest_element_in_a_stream_2.py
+++ src/kth-largest-element-in-a-stream/kth_largest_element_in_a_stream_2.py
@@ -4,7 +4,8 @@ import heapq
 class KthLargest:
 
     def __init__(self, k: int, nums: list[int]):
-        self.min_heap, self.k = nums, k
+        self.k = k
+        self.min_heap: list[int] = nums
         heapq.heapify(self.min_heap)
         while len(self.min_heap) > k:
             heapq.heappop(self.min_heap)
@@ -13,7 +14,7 @@ class KthLargest:
         heapq.heappush(self.min_heap, val)
         if len(self.min_heap) > self.k:
             heapq.heappop(self.min_heap)
-        return self.min_heap[0]    
+        return self.min_heap[0]
 
 
 # Your KthLargest object will be instantiated and called as such:
