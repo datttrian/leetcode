45f42798b4e70cb8411f58ec9e6f202d688b243a
datttrian <datttrian@gmail.com>
2024-07-11 23:24:00 +0000

    Thu, Jul 11, 2024, 11:24 PM +00:00

--- src/kth-largest-element-in-a-stream/kth_largest_element_in_a_stream_1.py
+++ src/kth-largest-element-in-a-stream/kth_largest_element_in_a_stream_1.py
@@ -17,5 +17,11 @@ class KthLargest:
 
 
 # Your KthLargest object will be instantiated and called as such:
-# obj = KthLargest(k, nums)
-# param_1 = obj.add(val)
+k = 3
+nums = [4, 5, 8, 2]
+obj = KthLargest(k, nums)
+print(obj.add(3))
+print(obj.add(5))
+print(obj.add(10))
+print(obj.add(9))
+print(obj.add(4))
--- src/kth-largest-element-in-a-stream/kth_largest_element_in_a_stream_2.py
+++ src/kth-largest-element-in-a-stream/kth_largest_element_in_a_stream_2.py
@@ -1,20 +1,12 @@
-import heapq
-
-
 class KthLargest:
-
     def __init__(self, k: int, nums: list[int]):
         self.k = k
-        self.min_heap: list[int] = nums
-        heapq.heapify(self.min_heap)
-        while len(self.min_heap) > k:
-            heapq.heappop(self.min_heap)
+        self.nums = sorted(nums, reverse=True)
 
     def add(self, val: int) -> int:
-        heapq.heappush(self.min_heap, val)
-        if len(self.min_heap) > self.k:
-            heapq.heappop(self.min_heap)
-        return self.min_heap[0]
+        self.nums.append(val)
+        self.nums.sort(reverse=True)
+        return self.nums[self.k - 1]
 
 
 # Your KthLargest object will be instantiated and called as such:
