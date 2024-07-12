0bfd8893349dbd4ade0bb65fda7b937559872630
datttrian <datttrian@gmail.com>
2024-07-12 00:16:23 +0000

    Fri, Jul 12, 2024, 12:16 AM +00:00

--- src/last-stone-weight/last_stone_weight_2.py
+++ src/last-stone-weight/last_stone_weight_2.py
@@ -1,15 +1,10 @@
-from bisect import insort
-
-
 class Solution:
     def lastStoneWeight(self, stones: list[int]) -> int:
-        stones.sort()
-
         while len(stones) > 1:
-            x = stones.pop()
-            y = stones.pop()
-
+            stones.sort(reverse=True)
+            x = stones.pop(0)
+            y = stones.pop(0)
             if x != y:
-                insort(stones, x - y)
+                stones.append(x - y)
 
         return stones[0] if stones else 0
