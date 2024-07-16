2992f882ddd98aecc2bcbf6abc05f01436b0262f
Dat Thanh Tran <datttrian@gmail.com>
2024-07-16 23:14:27 +0000

    Tue, Jul 16, 2024, 11:14 PM +00:00

--- src/step-by-step-directions-from-a-binary-tree-node-to-another/main.py
+++ src/step-by-step-directions-from-a-binary-tree-node-to-another/main.py
@@ -14,87 +14,38 @@ class TreeNode:
         self.right = right
 
 
-def list_to_tree(values: list[Optional[int]]) -> Optional[TreeNode]:
-    if not values:
-        return None
-    root = TreeNode(values[0])
-    queue = [root]
-    i = 1
-    while i < len(values):
-        current = queue.pop(0)
-        if values[i] is not None:
-            current.left = TreeNode(values[i])
-            queue.append(current.left)
-        i += 1
-        if i < len(values) and values[i] is not None:
-            current.right = TreeNode(values[i])
-            queue.append(current.right)
-        i += 1
-    return root
+class Solution:
+    def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
+        graph = defaultdict(list)
+        queue = deque([root])
 
+        while queue:
+            node = queue.popleft()
 
-def tree_to_list(root: Optional[TreeNode]) -> list[Optional[int]]:
+            if node.left:
+                graph[node.left.val].append((node.val, 'U'))
+                graph[node.val].append((node.left.val, 'L'))
+                queue.append(node.left)
 
-    result: list[Optional[int]] = []
-    queue: deque[Optional[TreeNode]] = deque([root])
+            if node.right:
+                graph[node.right.val].append((node.val, 'U'))
+                graph[node.val].append((node.right.val, 'R'))
+                queue.append(node.right)
 
-    while queue:
-        current = queue.popleft()
-        if current:
-            result.append(current.val)
-            queue.append(current.left)
-            queue.append(current.right)
-        else:
-            result.append(None)
+        queue = deque([(startValue, "")])
+        seen = set()
 
-    while result and result[-1] is None:
-        result.pop()
+        while queue:
+            cur_val, cur_path = queue.popleft()
 
-    return result
+            if cur_val in seen:
+                continue
 
+            seen.add(cur_val)
 
-startValue = 3
-destValue = 6
-t = list_to_tree([5, 1, 2, 3, None, 6, 4])
-
-
-graph = defaultdict(list)
-queue = deque([t])
-
-while queue:
-    node = queue.popleft()
-
-    if node.left:
-        graph[node.left.val].append((node.val, 'U'))
-        graph[node.val].append((node.left.val, 'L'))
-        queue.append(node.left)
-
-    if node.right:
-        graph[node.right.val].append((node.val, 'U'))
-        graph[node.val].append((node.right.val, 'R'))
-        queue.append(node.right)
-
-
-queue = deque([(startValue, "")])
-seen = set()
-
-cur_val, cur_path = queue.popleft()
-seen.add(cur_val)
-
-for child, direction in graph[cur_val]:
-    if child not in seen:
-        queue.append((child, cur_path + direction))
-
-cur_val, cur_path = queue.popleft()
-print(queue, seen, cur_val, cur_path)
-
-cur_val, cur_path = queue.popleft()
-
-seen.add(cur_val)
-
-for child, direction in graph[cur_val]:
-    if child not in seen:
-        queue.append((child, cur_path + direction))
-
-cur_val, cur_path = queue.popleft()
-print(queue, seen, cur_val, cur_path)
+            if cur_val == destValue:
+                return cur_path
+            else:
+                for child, direction in graph[cur_val]:
+                    if child not in seen:
+                        queue.append((child, cur_path + direction))
