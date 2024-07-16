8f9655461f4358295f25437c7e5f1161e23f1fd8
Dat Thanh Tran <datttrian@gmail.com>
2024-07-16 21:52:22 +0000

    Tue, Jul 16, 2024, 9:52 PM +00:00

---
+++ src/step-by-step-directions-from-a-binary-tree-node-to-another/main.py
@@ -0,0 +1,61 @@
+from collections import deque
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(
+        self,
+        val: Optional[int] = 0,
+        left: Optional["TreeNode"] = None,
+        right: Optional["TreeNode"] = None,
+    ) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def list_to_tree(values: list[Optional[int]]) -> Optional[TreeNode]:
+    if not values:
+        return None
+    root = TreeNode(values[0])
+    queue = [root]
+    i = 1
+    while i < len(values):
+        current = queue.pop(0)
+        if values[i] is not None:
+            current.left = TreeNode(values[i])
+            queue.append(current.left)
+        i += 1
+        if i < len(values) and values[i] is not None:
+            current.right = TreeNode(values[i])
+            queue.append(current.right)
+        i += 1
+    return root
+
+
+def tree_to_list(root: Optional[TreeNode]) -> list[Optional[int]]:
+
+    result: list[Optional[int]] = []
+    queue: deque[Optional[TreeNode]] = deque([root])
+
+    while queue:
+        current = queue.popleft()
+        if current:
+            result.append(current.val)
+            queue.append(current.left)
+            queue.append(current.right)
+        else:
+            result.append(None)
+
+    while result and result[-1] is None:
+        result.pop()
+
+    return result
+
+
+root = [5, 1, 2, 3, None, 6, 4]
+startValue = 3
+destValue = 6
+
+t = list_to_tree(root)
+print(t)
