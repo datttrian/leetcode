1937f3ef07914c979d4547aa7c0a24073e6e49b7
Dat Thanh Tran <datttrian@gmail.com>
2024-07-17 00:00:09 +0000

    Wed, Jul 17, 2024, 12:00 AM +00:00

--- src/step-by-step-directions-from-a-binary-tree-node-to-another/main.py
+++ src/step-by-step-directions-from-a-binary-tree-node-to-another/main.py
@@ -1,8 +1,6 @@
 from collections import defaultdict, deque
 from typing import Optional
 
-# Definition for a binary tree node.
-
 
 class TreeNode:
     def __init__(
@@ -20,16 +18,13 @@ class Solution:
     def getDirections(
         self, root: Optional[TreeNode], startValue: int, destValue: int
     ) -> str:
-        # Initialize an empty graph using a dictionary where each node will map to a list of tuples of a node value and a direction
         graph: dict[int, list[tuple[int, str]]] = defaultdict(list)
 
-        # Perform a breadth-first search (BFS) to build the graph representation of the tree
         queue: deque[Optional[TreeNode]] = deque([root])
         while queue:
             node = queue.popleft()
 
             if node is not None:
-                # Add a child and its relationship to the graph and queue
                 if node.left:
                     graph[node.left.val].append((node.val, "U"))
                     graph[node.val].append((node.left.val, "L"))
@@ -39,30 +34,22 @@ class Solution:
                     graph[node.val].append((node.right.val, "R"))
                     queue.append(node.right)
 
-        # Use a set to keep track of visited nodes to avoid revisiting them
         seen: set[int] = set()
 
-        # Perform a BFS to find the shortest path from startValue to destValue
         search_queue: deque[tuple[int, str]] = deque([(startValue, "")])
         while search_queue:
             cur_val, cur_path = search_queue.popleft()
 
-            # Skip the nodes has already been visited
             if cur_val in seen:
                 continue
 
-            # Mark the current node as visited
             seen.add(cur_val)
 
-            # If we have reached the destination node, return the path taken
             if cur_val == destValue:
                 return cur_path
 
-            # For each child of the current node, if it hasn't been visited, add it to the search queue
             for child, direction in graph[cur_val]:
                 if child not in seen:
-                    # Append the child node and the updated path (with the direction) to the search queue
                     search_queue.append((child, cur_path + direction))
 
-        # If no path is found, return an empty string
         return ""
