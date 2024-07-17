49b877e96c5a5bfc47159eb0e64f4f5cc577c051
datttrian <datttrian@gmail.com>
2024-07-17 17:50:40 +0000

    Wed, Jul 17, 2024, 5:50 PM +00:00

--- src/reorder-list/main.py
+++ src/reorder-list/main.py
@@ -29,3 +29,35 @@ def linked_list_to_list(node: Optional[ListNode]) -> list[int]:
         result.append(node.val)
         node = node.next
     return result
+
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head or not head.next:
+            return
+
+        slow, fast = head, head.next
+
+        while slow.next and fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second = slow.next
+        prev = slow.next = None
+
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+
+        first, second = head, prev
+        while first.next and second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first = tmp1
+            second = tmp2
