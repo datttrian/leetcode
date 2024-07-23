7b313e3815be485f302805c2d7c5fd9e0920865b
datttrian <datttrian@gmail.com>
2024-07-23 23:00:11 +0000

    Tue, Jul 23, 2024, 11:00 PM +00:00

--- src/sort-array-by-increasing-frequency/sort_array_by_increasing_frequency.py
+++ src/sort-array-by-increasing-frequency/sort_array_by_increasing_frequency.py
@@ -3,6 +3,7 @@ from collections import Counter, defaultdict
 
 class Solution:
     def frequencySort(self, nums: list[int]) -> list[int]:
+
         # Count the frequency of each number in the input list
         nums_count: Counter[int] = Counter(nums)
 
@@ -15,11 +16,11 @@ class Solution:
         for num_list in group_count.values():
             num_list.sort(reverse=True)
 
-        # Loop through the groups of sorted frequency
+        # Loop through the groups of sorted frequency in ascending order
         result: list[int] = []
         for count, num_list in sorted(group_count.items()):
 
-            # For each number in the group
+            # Loop through all numbers in the group
             for num in num_list:
 
                 # Append the number count times to the result list
