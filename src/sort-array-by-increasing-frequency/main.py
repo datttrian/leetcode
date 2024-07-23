2090c4fa409184c7c8ca2d179a60e55fe5dd9b27
datttrian <datttrian@gmail.com>
2024-07-23 22:07:13 +0000

    Tue, Jul 23, 2024, 10:07 PM +00:00

--- src/sort-array-by-increasing-frequency/main.py
+++ src/sort-array-by-increasing-frequency/main.py
@@ -5,3 +5,8 @@ nums = [7, 1, 4, 1, 7, 1, 4, 4, 2, 2, 2, 3, 7, 3, 3, 5, 5, 5, 5, 5, 8, 8, 8, 6, 
 nums_count = Counter(nums)
 print(nums_count)
 
+group_count = {}
+
+for num, freq in freq_dict.items():
+    if freq in freq_group:
+        freq_group[freq].append(n)
