937549f4ff1ce8848fdf75528e45d70c80fc1ed0
datttrian <datttrian@gmail.com>
2024-07-23 22:03:37 +0000

    Tue, Jul 23, 2024, 10:03 PM +00:00

--- src/sort-array-by-increasing-frequency/main.py
+++ src/sort-array-by-increasing-frequency/main.py
@@ -1,1 +1,7 @@
+from collections import Counter
+
 nums = [7, 1, 4, 1, 7, 1, 4, 4, 2, 2, 2, 3, 7, 3, 3, 5, 5, 5, 5, 5, 8, 8, 8, 6, 6, 6, 6, 6, 6, 9, 9, 9, 9]
+
+nums_count = Counter(nums)
+print(nums_count)
+
