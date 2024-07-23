ecd66ab133c4c5c18f666c2aa2c2b8951125a181
datttrian <datttrian@gmail.com>
2024-07-23 22:43:03 +0000

    Tue, Jul 23, 2024, 10:43 PM +00:00

--- src/sort-array-by-increasing-frequency/main.py
+++ src/sort-array-by-increasing-frequency/main.py
@@ -5,16 +5,13 @@ class Solution:
     def frequencySort(self, nums: list[int]) -> list[int]:
         nums_count = Counter(nums)
         group_count = defaultdict(list)
-        result = []
 
         [group_count[count].append(num) for num, count in nums_count.items()]
 
         for _, count in group_count.items():
             count.sort(reverse=True)
 
-        for count, nums in sorted(group_count.items()):
-            for num in nums:
-                for _ in range(count):
-                    result.append(num)
+        result = [num for count in sorted(
+            group_count) for num in group_count[count] for _ in range(count)]
 
         return result
