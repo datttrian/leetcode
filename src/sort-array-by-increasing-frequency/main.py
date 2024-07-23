b2afe5a14c138fa742d10e79dfae7c896992a3f8
datttrian <datttrian@gmail.com>
2024-07-23 22:33:13 +0000

    Tue, Jul 23, 2024, 10:33 PM +00:00

--- src/sort-array-by-increasing-frequency/main.py
+++ src/sort-array-by-increasing-frequency/main.py
@@ -1,33 +1,22 @@
-from collections import Counter
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        nums_count = Counter(nums)
+        group_count = {}
 
-nums = [7, 1, 4, 1, 7, 1, 4, 4, 2, 2, 2, 3, 7, 3, 3, 5, 5, 5, 5, 5, 8, 8, 8, 6, 6, 6, 6, 6, 6, 9, 9, 9, 9]
+        for num, count in nums_count.items():
+            if count in group_count:
+                group_count[count].append(num)
+            else:
+                group_count[count] = [num]
+        
+        for group, count in group_count.items():
+            count.sort(reverse=True)
 
-nums_count = Counter(nums)
-print(nums_count)
-
-group_count = {}
-
-for num, count in nums_count.items():
-    if count in group_count:
-        group_count[count].append(num)
-    else:
-        group_count[count] = [num]
-
-
-
-print(group_count.keys())
-
-for group, count in group_count.items():
-    count.sort(reverse=True)
-
-print(group_count)
-print(sorted(group_count))
-
-result = []
-for count, nums in sorted(group_count.items()):
-    for num in nums:
-        for _ in range(count):
-            result.append(num)
-
-
-print(result)
+        result = []
+        
+        for count, nums in sorted(group_count.items()):
+            for num in nums:
+                for _ in range(count):
+                    result.append(num)
+        
+        return result
