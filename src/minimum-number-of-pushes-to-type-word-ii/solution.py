5e061958fa32689310c23283b3ac5ea4d6fad400
datttrian <datttrian@gmail.com>
2024-08-06 22:35:25 +0000

    Tue, Aug 6, 2024, 10:35 PM +00:00

--- src/minimum-number-of-pushes-to-type-word-ii/solution.py
+++ src/minimum-number-of-pushes-to-type-word-ii/solution.py
@@ -3,6 +3,7 @@ from collections import Counter
 
 class Solution:
     def minimumPushes(self, word: str) -> int:
+
         # Count the occurrences of each character in `word` using `Counter`
         character_counts = Counter(word)
 
@@ -12,9 +13,11 @@ class Solution:
         # Loop through the sorted counts with their indices.
         total_pushes = 0
         for index, count in enumerate(sorted_counts):
-            # Calculate the multiplier for every 8 characters based on the index. Every 8 characters increase the multiplier by 1.
+
+            # Calculate the multiplier for every 8 characters based on the index
             multiplier = index // 8 + 1
-            # Add the product of the count and the multiplier to the total pushes.
+
+            # Add the product of the count and the multiplier to the total pushes
             total_pushes += count * multiplier
 
         # Return the total number of pushes required.
