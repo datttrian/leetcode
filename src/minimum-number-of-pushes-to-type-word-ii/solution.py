d932c9a3982954aefee8cdb669ecf6a643b147fb
datttrian <datttrian@gmail.com>
2024-08-06 22:29:35 +0000

    Tue, Aug 6, 2024, 10:29 PM +00:00

--- src/minimum-number-of-pushes-to-type-word-ii/solution.py
+++ src/minimum-number-of-pushes-to-type-word-ii/solution.py
@@ -1,28 +1,26 @@
 from collections import Counter
 
-word = "aaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbccccddddeeeeffffgggghhhhiiiiijjjjjkkkkkllllmmmmnnnnoooopppppqqqqrrrrsssstttttuuuuvvvvwwwwxxyyzzz"
 
-counts = Counter(word)
+class Solution:
+    def minimumPushes(self, word: str) -> int:
 
-queue = list(counts.values())
-queue.sort(reverse=True)
+        counts = Counter(word)
 
+        queue = list(counts.values())
+        queue.sort(reverse=True)
 
+        push_count = 0
 
+        for key, value in enumerate(queue):
+            push_count += value
 
+            if key >= 8:
+                push_count += value
 
-push_count = 0
+            if key >= 16:
+                push_count += value
 
-for key, value in enumerate(queue):
-    push_count += value
+            if key >= 24:
+                push_count += value
 
-    if key >= 8:
-        push_count += value
-
-    if key >= 16:
-        push_count += value
-
-    if key >= 24:
-        push_count += value
-
-
+        return push_count
