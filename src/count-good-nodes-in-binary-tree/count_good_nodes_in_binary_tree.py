de8501ee42252200cbb026c00bb74bc3a75f029e
datttrian <datttrian@gmail.com>
2024-08-22 22:17:41 +0000

    Thu, Aug 22, 2024, 6:17 PM +00:00

--- src/count-good-nodes-in-binary-tree/count_good_nodes_in_binary_tree.py
+++ src/count-good-nodes-in-binary-tree/count_good_nodes_in_binary_tree.py
@@ -13,4 +13,10 @@ class Solution:
                 return 0
 
             res = 1 if node.val >= maxVal else 0
-            maxVal max(maxVal, node.val)
+            maxVal = max(maxVal, node.val)
+            res += dfs(node.left, maxVal)
+            res += dfs(node.right, maxVal)
+
+            return res
+        
+        return dfs(root, root.val)
